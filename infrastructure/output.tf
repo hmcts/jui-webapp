daef2d0298ca73a6c0a649f37c97438b726041de
Alec Doran-Twyford <alec@alectronic.co>
2018-04-30 15:42:10 +0100

    comment our vault for now

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,14 +1,14 @@
 output "microserviceName" {
-  value = "${var.product}-${var.component}"
+  value = "${local.app_full_name}"
 }
 
-output "vaultName" {
-  value = "${module.key_vault.key_vault_name}"
-}
-
-output "vaultUri" {
-  value = "${module.key_vault.key_vault_uri}"
-}
+//output "vaultName" {
+//  value = "${module.key_vault.key_vault_name}"
+//}
+//
+//output "vaultUri" {
+//  value = "${module.key_vault.key_vault_uri}"
+//}
 
 output "idam_api_url" {
   value = "${var.idam_api_url}"
