fa8eae078985ce08a04ec85f17de806c77496402
Alec Doran-Twyford <alec@alectronic.co>
2018-04-30 15:50:08 +0100

    revert keyvault change

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -2,13 +2,13 @@ output "microserviceName" {
   value = "${local.app_full_name}"
 }
 
-//output "vaultName" {
-//  value = "${module.key_vault.key_vault_name}"
-//}
-//
-//output "vaultUri" {
-//  value = "${module.key_vault.key_vault_uri}"
-//}
+output "vaultName" {
+  value = "${module.key_vault.key_vault_name}"
+}
+
+output "vaultUri" {
+  value = "${module.key_vault.key_vault_uri}"
+}
 
 output "idam_api_url" {
   value = "${var.idam_api_url}"
