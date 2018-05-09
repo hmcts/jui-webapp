cc225b3b2f18c62b13449d41718f8fdd0866395c
Roopa Devi <roopa@Roopas-MacBook-Pro.local>
2018-05-09 12:56:06 +0100

    reverted changes of config file

--- cucumber-e2e/config/config.js
+++ cucumber-e2e/config/config.js
@@ -5,13 +5,18 @@ chai.use(chaiAsPromised);
 exports.config = {
   seleniumAddress: "http://localhost:4444/wd/hub",
   baseUrl: "http://localhost:3608",
+
   capabilities: {
     browserName: "chrome"
   },
+
+
   framework: "custom",
   frameworkPath: require.resolve("protractor-cucumber-framework"),
   specs: ["../features/*.feature"],
+
   resultJsonOutputFile: "reports/json/protractor_report.json",
+
   onPrepare: function() {
     browser.manage().window().maximize();
     browser.waitForAngularEnabled(false);
@@ -19,6 +24,8 @@ exports.config = {
     global.assert = chai.assert;
     global.should = chai.should;
     },
+
+
   cucumberOpts: {
     strict: true,
     format: ["pretty"],
