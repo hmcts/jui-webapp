91091d42b64f66af04b3a560ad382b00c9766a09
mike-suggitt <mikessuggitt@gmail.com>
2018-06-28 15:20:16 +0100

    - Added e2e tests

--- test/e2e/features/step_definitions/login.steps.js
+++ test/e2e/features/step_definitions/login.steps.js
@@ -5,8 +5,8 @@ Given(/^I login$/, async function () {
     const username = $(this.getSelector('idam-username'));
     const password = $(this.getSelector('idam-password'));
     const submit = $(this.getSelector('idam-submit'));
-    await username.sendKeys(process.env.BDD_USERNAME);
-    await password.sendKeys(process.env.BDD_PASSWORD);
+    await username.sendKeys(this.config.username);
+    await password.sendKeys(this.config.password);
     await submit.click();
     await browser.wait(() => {
         return $(this.getSelector('jui-header')).isPresent();
