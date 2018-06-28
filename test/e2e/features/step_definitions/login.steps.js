c0d031e7faee0e40d478a5c05ea3de4c6a4dd883
mike-suggitt <mikessuggitt@gmail.com>
2018-06-28 15:12:05 +0100

    - Added e2e tests

--- test/e2e/features/step_definitions/login.steps.js
+++ test/e2e/features/step_definitions/login.steps.js
@@ -5,8 +5,8 @@ Given(/^I login$/, async function () {
     const username = $(this.getSelector('idam-username'));
     const password = $(this.getSelector('idam-password'));
     const submit = $(this.getSelector('idam-submit'));
-    await username.sendKeys('juitestuser1@gmail.com');
-    await password.sendKeys('Monday01');
+    await username.sendKeys(process.env.BDD_USERNAME);
+    await password.sendKeys(process.env.BDD_PASSWORD);
     await submit.click();
     await browser.wait(() => {
         return $(this.getSelector('jui-header')).isPresent();
