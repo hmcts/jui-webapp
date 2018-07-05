7b58ccaac7964d9772227925ba547a3c644cc9b3
mike-suggitt <mikessuggitt@gmail.com>
2018-07-05 09:18:06 +0100

    - Removed ngxcookie workaround now that we're not using removeAll

--- server.js
+++ server.js
@@ -32,8 +32,6 @@ app.use(express.static(__dirname + '/dist', { index: false }));
 
 app.get('/*', (req, res) => {
     console.time(`GET: ${req.originalUrl}`);
-
-    res.headers = res.getHeaders();
     res.render('./dist/index', {
         req: req,
         res: res,
