9c347eae0e5b81bb30618cfbd1ef78867a84ceb1
rramisetty <roopa.ramisetty@hmcts.net>
2018-07-10 09:23:21 +0100

    adding casesummarypage

--- cucumberjs/config/conf.js
+++ cucumberjs/config/conf.js
@@ -8,27 +8,26 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     getPageTimeout: 60000,
     allScriptsTimeout: 500000,
-    baseUrl:'',
+    baseUrl:'https://jui-webapp-aat.service.core-compute-aat.internal/',
 
-    capabilities: {
-        browserName: 'chrome',
-    },
+    // capabilities: {
+    //     browserName: 'chrome',
+    // },
 
 
     /*****
      * to enable proxy
      */
-    // capabilities: {
-    //     browserName: 'chrome',
-    //
-    //     'proxy': {
-    //
-    //         'proxyType': 'manual',
-    //         'httpProxy': 'proxyout.reform.hmcts.net:8080',  //'socks5://localhost:9090',
-    //         'sslProxy' : 'proxyout.reform.hmcts.net:8080',  //'socks5://localhost:9090',
-    //
-    //     }
-    // },
+    capabilities: {
+        browserName: 'chrome',
+        'proxy': {
+
+            proxyType: 'manual',
+            httpProxy: 'proxyout.reform.hmcts.net:8080',
+            sslProxy : 'proxyout.reform.hmcts.net:8080',
+            noProxy: 'localhost:3000'
+        }
+    },
 
 
 
@@ -52,6 +51,6 @@ exports.config = {
         strict: true,
         format: ['pretty'],
         require: ['../features/step_definitions/*.js', '../support/*.js'],
-        tags: ''
+        tags: '@wip'
     }
 };
--- cucumberjs/features/step_definitions/dashBoard_steps.js
+++ cucumberjs/features/step_definitions/dashBoard_steps.js
@@ -4,81 +4,88 @@ var logInPage = require('../../pages/logInPage');
 var dashBoardPage = require('../../pages/dashBoardPage');
 var caseSummaryPage = require('../../pages/caseSummaryPage');
 
+var chai = require('chai');
+var expect = chai.expect;
 var expect = require('chai').expect;
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+chai.use(require('chai-smoothie'));
+var assert = require('chai').assert;
+
 var assert = require('assert');
 var conf = require('../../config/conf').config;
 var {defineSupportCode} = require('cucumber');
-var chai = require('chai');
-chai.use(require('chai-smoothie'));
-//var chaiAsPromised = require('chai-as-promised');
 
 
 defineSupportCode(function ({Given, When, Then}) {
 
-    Given(/^I am logged in as a Judge$/, async function () {
-        await logInPage.email.sendKeys('test@test.com');
-        await logInPage.password.sendKeys('123');
-        await logInPage.signin_btn.click();
-
-    });
-
-    When(/^I am on the dashboard page$/, async function () {
-        var dashboard_header_text = dashBoardPage.dashboard_header;
-        await expect(dashboard_header_text).to.be.present;
-    });
-
-
-    When(/^one or more cases are displayed$/, async function () {
-        await dashBoardPage.table.isPresent();
-
-
-        var num_rows = dashBoardPage.number_of_rows.count().then(function (rowCount) {
-            console.log('Count:' + rowCount)
-
+    Given(/^I am logged in as a Judge$/, async function() {
+       await logInPage.email.sendKeys('Stuart.Plummer@hmcts.net');
+      await logInPage.password.sendKeys('Monday01');
+      await logInPage.signin_btn.click();
+        browser.driver.sleep(1000);
+        browser.waitForAngular();
         });
 
-        var num_case_links = dashBoardPage.case_number_links.count().then(function (linkCount) {
-            console.log('Count:' + linkCount)
-        });
+    When(/^I am on the dashboard page$/, (next) => {
+        browser.driver.sleep(1000);
+        browser.waitForAngular();
+        expect(dashBoardPage.dashboard_header.isDisplayed()).to.eventually.be.true.and.notify(next);
+        expect(dashBoardPage.dashboard_header.getText()).to.eventually.equal('Dashboard').and.notify(next);
+        browser.driver.sleep(1000);
+        browser.waitForAngular();
+        next();
+    });
 
-        expect(num_rows).to.eventually.equal(num_case_links);
+
+    When(/^one or more cases are displayed$/, (next) => {
+        browser.driver.sleep(1000);
+        browser.waitForAngular();
+        expect(dashBoardPage.table.isDisplayed()).to.eventually.be.true.and.notify(next);
+        var num_rows = dashBoardPage.number_of_rows.count();
+        var num_case_links = dashBoardPage.case_number_links.count();
+        assert.equal(num_rows, num_case_links, 'no table or table?');
+        browser.debugger();
+        browser.driver.sleep(1000);
+        browser.waitForAngular();
+        next();
 
     });
 
 
-    When(/^all case numbers are hyperlinked$/, async function () {
+    When(/^all case numbers are hyperlinked$/, (next) => {
+        browser.driver.sleep(1000);
+        browser.waitForAngular();
         var case_nums = dashBoardPage.case_number_links;
-        case_nums.getAttribute('href').isPresent();
+        expect(case_nums.first().getAttribute('href').isPresent()).to.eventually.be.true.and.notify(next);
+        var text = dashBoardPage.case_number_links.first().getText();
+        debugger;
+        expect(case_nums.first().getAttribute('href')).to.eventually.equal(conf.baseUrl + /viewcase/ + text + /summary/).and.notify(next);
 
-
-        //await expect(case_nums.first().getAttribute('href')).to.eventually.equal(`${conf.baseUrl}/viewcase/${first_case_num}/summary/`);
-       // var regexString = 'http:\\/\\/localhost:3000\\/viewcase\\/(\\d+)\\/summary';
-       // var  str = 'http://localhost:3000/viewcase/1530617412130963/summary';
-       //  var regex = RegExp(regexString);
-       //  await console.log(regex.test(str));
+        browser.driver.sleep(1000);
+        browser.waitForAngular();
+        next();
     });
 
 
-
-    //     var link_text = case_nums.first().getAttribute('href').then(async function (href) {
-    //         console.log(href);
-    //         return link_text;
-    //     });
-    //     expect(link_text).to.eventually.equal(conf.baseUrl /+/viewcase/+/{href}/+/summary/);
-    // });
-
-
-
-
-    When(/^I select a case reference$/,async function () {
-         await dashBoardPage.case_number_links.first().click();
-         });
+    When(/^I select a case reference$/, (next) => {
+        browser.driver.sleep(1000);
+        browser.waitForAngular();
+        dashBoardPage.case_number_links.first().click();
+        browser.driver.sleep(1000);
+        browser.waitForAngular();
+        next();
+    });
 
 
-
-    Then(/^I will be redirected to the Case Summary page for that case$/, async function () {
+    Then(/^I will be redirected to the Case Summary page for that case$/, (next) => {
+        browser.driver.sleep(1000);
+        browser.waitForAngular();
         var case_summary_header_text = caseSummaryPage.caseDetails_header_text;
-        await expect(case_summary_header_text.first()).to.be.present;
-        });
+        expect(case_summary_header_text.first().isDisplayed()).to.eventually.be.true.and.notify(next);
+        browser.driver.sleep(1000);
+        browser.waitForAngular();
+
+    });
 
 });
