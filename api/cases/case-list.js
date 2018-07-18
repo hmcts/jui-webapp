23492bf02d47ba75eef23c8b2cab6b548f79a300
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2018-07-18 16:27:36 +0100

    RIUI : 653 View case status for all SSCS cases
    - Added functionality to get status from COH and display on the UI.

--- api/cases/case-list.js
+++ api/cases/case-list.js
@@ -7,6 +7,10 @@ function getCases(userId, options, caseType = 'Benefit', caseStateId = 'appealCr
     return generateRequest(`${config.services.ccd_data_api}/caseworkers/${userId}/jurisdictions/${jurisdiction}/case-types/${caseType}/cases?${benefitType}state=${caseStateId}&page=1&sortDirection=DESC`, options)
 }
 
+function getOnlineHearing(caseIds, options) {
+    return generateRequest(`${config.services.coh_cor_api}/continuous-online-hearings/?case_id=${caseIds}`, options);
+}
+
 function rawCasesReducer(cases, columns) {
     return cases.map(caseRow => {
         return {
@@ -24,17 +28,34 @@ function rawCasesReducer(cases, columns) {
 module.exports = (req, res, next) => {
     const token = req.auth.token;
     const userId = req.auth.userId;
-
-    getCases(userId, {
-        headers : {
-            'Authorization' : `Bearer ${token}`,
-            'ServiceAuthorization' : req.headers.ServiceAuthorization
+    const options = {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'ServiceAuthorization': req.headers.ServiceAuthorization
         }
-    }).then(casesData => {
+    };
+
+    getCases(userId, options)
+        .then(caseData => {
+                let caseIds = caseData.map(caseRow => 'case_id=' + caseRow.id).join("&");
+                let casesData = getOnlineHearing(caseIds, options)
+                .then(hearings => {
+                    if (hearings.online_hearings) {
+                        let caseStateMap = new Map(hearings.online_hearings.
+                        map(hearing => [Number(hearing.case_id), hearing.current_question_state.state_name]));
+                        caseData.forEach(caseRow => {
+                            caseRow.status = caseStateMap.get(Number(caseRow.id));
+                        });
+                    }
+                    return caseData;
+                });
+                return casesData;
+            })
+        .then(casesData => {
         let results = rawCasesReducer(casesData, sscsCaseListTemplate.columns).sort(function (result1, result2) {
-            return new Date(result1.case_fields.dateOfLastAction) - new Date(result2.case_fields.dateOfLastAction) ;
+            return new Date(result1.case_fields.dateOfLastAction) - new Date(result2.case_fields.dateOfLastAction);
         });
-        const aggregatedData = {...sscsCaseListTemplate, results : results};
+        const aggregatedData = {...sscsCaseListTemplate, results: results};
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('content-type', 'application/json');
         res.status(200).send(JSON.stringify(aggregatedData));
