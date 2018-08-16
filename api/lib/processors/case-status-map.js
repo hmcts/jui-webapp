29b17fa1b71acde7133eb0dc8247cdcf1725460b
shaibazkhan <sknizami@gmail.com>
2018-08-16 14:53:08 +0100

    simplified case status mapping

---
+++ api/lib/processors/case-status-map.js
@@ -0,0 +1,15 @@
+module.exports = {
+    sscs: {
+        benefit: {}
+    },
+    cmc: {
+        moneyclaimcase: {}
+    },
+    probate: {
+        grantofrepresentation: {}
+    },
+    divorce: {
+        divorce: {},
+        financialremedymvp2: { consentOrderMade: 'Consent Order Made' }
+    }
+};
