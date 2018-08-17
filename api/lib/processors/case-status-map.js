ea076c93273cbc6cfa51d2e42c309a2f8caa87ff
shaibazkhan <sknizami@gmail.com>
2018-08-17 11:00:59 +0100

    added some more states

--- api/lib/processors/case-status-map.js
+++ api/lib/processors/case-status-map.js
@@ -2,7 +2,9 @@ module.exports = {
     sscs: {
         benefit: {
             continuous_online_hearing_started: 'DWP response received',
-            questions_answered: 'Questions answered'
+            questions_answered: 'Appellant\'s answers received',
+            question_deadline_elapsed: 'Question deadline expired',
+            question_issued: 'Questions sent'
         }
     },
     cmc: {
