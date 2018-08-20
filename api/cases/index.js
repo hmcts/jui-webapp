81bcbc055a3defd95f0528076755f54f4dcf63b5
Alec Doran-Twyford <alec@alectronic.co>
2018-08-20 14:39:07 +0100

    /refactor-endpoints:

--- api/cases/index.js
+++ api/cases/index.js
@@ -1,17 +1,10 @@
-const jp = require('jsonpath');
 const express = require('express');
-
 const router = express.Router();
+
 const getCaseListCallback = require('./case-list');
 const getCaseCallback = require('./case');
-const events = require('../events');
-const questions = require('../questions');
 
 router.get('/', getCaseListCallback);
-router.get('/jurisdiction/:jur/casetype/:casetype/:case_id/events', events);
 router.get('/jurisdiction/:jur/casetype/:casetype/:case_id', getCaseCallback);
-router.post('/jurisdiction/:jur/casetype/:casetype/:case_id/questions', questions);
-router.patch('/jurisdiction/:jur/casetype/:casetype/:case_id/questions/:question_id', questions);
-router.delete('/jurisdiction/:jur/casetype/:casetype/:case_id/questions/:question_id', questions);
 
 module.exports = router;
