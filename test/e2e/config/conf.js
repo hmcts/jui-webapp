b54326cf307ffb582d28c894dfb3d4fed6f83437
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2018-08-22 16:52:53 +0100

    slight change in Jenkins_CNP

--- test/e2e/config/conf.js
+++ test/e2e/config/conf.js
@@ -33,7 +33,7 @@ const localConfig = [
     {
         browserName: 'chrome',
         acceptInsecureCerts: true,
-    //    chromeOptions: { args: ['--headless']},
+    //  chromeOptions: { args: ['--headless']},
         proxy: {
             proxyType: 'manual',
             httpProxy: 'proxyout.reform.hmcts.net:8080',
