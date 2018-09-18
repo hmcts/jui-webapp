91b78fd41fbb24d586c4d68bbb4d1ad4e01bc312
shaibazkhan <sknizami@gmail.com>
2018-09-18 16:04:23 +0100

    added unit test for question rounds states

---
+++ api/lib/case-state.spec.js
@@ -0,0 +1,140 @@
+const proxyquire = require('proxyquire').noPreserveCache();
+
+const questionAnswered = {
+    current_question_round: 0,
+    question_rounds: [
+        {
+            deadline_extension_count: 0,
+            question_references: [
+                {
+                    current_question_state: {
+                        state_datetime: '2018-09-11T14:04:17Z',
+                        state_desc: 'string',
+                        state_name: 'question_answered'
+                    },
+                    deadline_extension_count: 0
+                },
+                {
+                    current_question_state: {
+                        state_datetime: '2018-09-12T14:04:17Z',
+                        state_desc: 'string',
+                        state_name: 'question_answered'
+                    },
+                    deadline_extension_count: 0
+                }
+            ],
+            question_round_number: '0',
+            question_round_state: {
+                state_name: 'question_answered'
+            }
+        }
+    ]
+};
+
+const deadlineExtensionExpired = {
+    current_question_round: 0,
+    question_rounds: [
+        {
+            deadline_extension_count: 1,
+            question_references: [
+                {
+                    current_question_state: {
+                        state_datetime: '2018-09-11T14:04:17Z',
+                        state_desc: 'string',
+                        state_name: 'question_deadline_elapsed'
+                    },
+                    deadline_extension_count: 1
+                },
+                {
+                    current_question_state: {
+                        state_datetime: '2018-09-12T14:04:17Z',
+                        state_desc: 'string',
+                        state_name: 'question_answered'
+                    },
+                    deadline_extension_count: 0
+                }
+            ],
+            question_round_number: '0',
+            question_round_state: {
+                state_name: 'question_deadline_elapsed'
+            }
+        }
+    ]
+};
+
+describe('case-question-state', () => {
+    let route = {};
+    let onlineHearing = {};
+
+    beforeEach(() => {
+        onlineHearing = {
+            online_hearing_id: '1',
+            case_id: 987654321,
+            start_date: '2018-07-17T12:56:49.145+0000',
+            current_state: {
+                state_name: 'continuous_online_hearing_started',
+                state_datetime: '2018-07-17T12:56:49Z'
+            }
+        };
+    });
+
+    fdescribe('no question rounds exist', () => {
+        let getQuestionRoundState;
+
+        beforeEach(() => {
+            route = proxyquire('./case-state', {
+                '../questions': { getRounds: () => Promise.resolve() }
+            });
+
+            getQuestionRoundState = route.getQuestionRoundState;
+        });
+
+        it('should return case hearing status', () => {
+            getQuestionRoundState(onlineHearing, {}).then(val => {
+                expect(val).toEqual(onlineHearing.current_state);
+            });
+        });
+    });
+
+    fdescribe('question rounds exist with question answered', () => {
+        let getQuestionRoundState;
+
+        beforeEach(() => {
+            route = proxyquire('./case-state', {
+                '../questions': { getRounds: () => Promise.resolve(questionAnswered) }
+            });
+
+            getQuestionRoundState = route.getQuestionRoundState;
+        });
+
+        it('should return no case status', () => {
+            getQuestionRoundState(onlineHearing, {}).then(val => {
+                expect(val).toEqual({
+                    state_datetime: '2018-09-12T14:04:17Z',
+                    state_name: 'question_answered'
+                });
+            });
+        });
+    });
+
+    fdescribe('question rounds exist with expired deadline extension', () => {
+        let getQuestionRoundState;
+
+        beforeEach(() => {
+            route = proxyquire('./case-state', {
+                '../questions': { getRounds: () => Promise.resolve(deadlineExtensionExpired) }
+            });
+
+            getQuestionRoundState = route.getQuestionRoundState;
+        });
+
+        it('it should return case status', () => {
+            getQuestionRoundState(onlineHearing, {}).then(val => {
+                expect(val).toEqual({
+                    state_datetime: '2018-09-12T14:04:17Z',
+                    state_name: 'question_deadline_extension_elapsed'
+                });
+            });
+        });
+    });
+});
--- api/questions/index.js
+++ api/questions/index.js
@@ -1,8 +1,10 @@
 const questionsRoute = require('./question');
-const { postHearing, getAllQuestionsByCase } = require('./question');
+const { postHearing, getAllQuestionsByCase, getRounds } = require('./question');
 
 module.exports = app => questionsRoute(app);
 
 module.exports.postHearing = postHearing;
 
 module.exports.getAllQuestionsByCase = getAllQuestionsByCase;
+
+module.exports.getRounds = getRounds;
