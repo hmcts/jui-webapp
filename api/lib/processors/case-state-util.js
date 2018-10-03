1c57a32da214114e217b9bb518b5f54d3eee400e
shaibazkhan <sknizami@gmail.com>
2018-10-03 12:13:04 +0100

    making string double quote to see if it fixes codacy issues

--- api/lib/processors/case-state-util.js
+++ api/lib/processors/case-state-util.js
@@ -1,5 +1,5 @@
-const DECISION_ISSUED = 'continuous_online_hearing_decision_issued';
-const RELISTED = 'continuous_online_hearing_relisted';
+const DECISION_ISSUED = "continuous_online_hearing_decision_issued";
+const RELISTED = "continuous_online_hearing_relisted";
 
 function caseStateFilter(caseData) {
     const stateName = caseData.state.stateName;
@@ -15,14 +15,14 @@ function createCaseState(state, date, actionUrl) {
 }
 
 const CONSTANTS = {
-    COH_STATE: 'continuous_online_hearing_started',
-    Q_DEADLINE_ELAPSED_STATE: 'question_deadline_elapsed',
-    Q_DEADLINE_EXT_ELAPSED_STATE: 'question_deadline_extension_elapsed',
+    COH_STATE: "continuous_online_hearing_started",
+    Q_DEADLINE_ELAPSED_STATE: "question_deadline_elapsed",
+    Q_DEADLINE_EXT_ELAPSED_STATE: "question_deadline_extension_elapsed",
     DECISION_ISSUED_STATE: DECISION_ISSUED,
     RELISTED_STATE: RELISTED,
-    REFER_TO_JUDGE: 'referredToJudge',
-    QUESTIONS_GO_TO: 'questions',
-    CASE_FILE_GO_TO: 'casefile'
+    REFER_TO_JUDGE: "referredToJudge",
+    QUESTIONS_GO_TO: "questions",
+    CASE_FILE_GO_TO: "casefile"
 };
 
 module.exports.CONSTANTS = CONSTANTS;
