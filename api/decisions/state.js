7412b00d02d31a02a61a2c496df74130f1fda7de
Alan McCabe-Jones <alan.work@teamsmog.com>
2018-10-10 16:07:53 +0100

    utility commit

--- api/decisions/state.js
+++ api/decisions/state.js
@@ -1,10 +1,14 @@
-const Store = require('../lib/store');
-const generateRequest = require('../lib/request');
-const config = require('../../config');
-const express = require('express');
-const stateMeta = require('./state_meta');
+const Store = require('../lib/store')
+const generateRequest = require('../lib/request')
+const config = require('../../config')
+const express = require('express')
+const stateMeta = require('./state_meta')
 
-const ERROR404 = 404;
+const ERROR404 = 404
+
+const translate = {
+    notesForAdmin: 'orderDirectionAddComments'
+}
 
 function getOptions(req) {
     return {
@@ -12,31 +16,31 @@ function getOptions(req) {
             Authorization: `Bearer ${req.auth.token}`,
             ServiceAuthorization: req.headers.ServiceAuthorization
         }
-    };
+    }
 }
 
 async function getCaseWithEventToken(userId, jurisdiction, caseType, caseId, eventId, options) {
-    let response = await generateRequest(
+    const response = await generateRequest(
         'GET',
         `${
             config.services.ccd_data_api
         }/caseworkers/${userId}/jurisdictions/${jurisdiction}/case-types/${caseType}/cases/${caseId}/event-triggers/${eventId}/token`,
         options
-    );
-    return { token: response.token, caseDetails: response.case_details };
+    )
+    return { token: response.token, caseDetails: response.case_details }
 }
 
-async function postCaseWithEventToken(userId,jurisdiction,caseType,caseId,options)
+async function postCaseWithEventToken(userId, jurisdiction, caseType, caseId, options) {}
 
 /* eslint-disable-next-line complexity */
 function handlePostState(req, res, responseJSON, theState) {
-    const store = new Store(req);
-    const inCaseId = req.params.case_id;
+    const store = new Store(req)
+    const inCaseId = req.params.case_id
 
-    const formValues = req.body.formValues;
+    const formValues = req.body.formValues
 
     if (formValues) {
-        store.set(`decisions_${inCaseId}`, formValues);
+        store.set(`decisions_${inCaseId}`, formValues)
     }
 
     /* eslint-disable indent */
@@ -44,42 +48,42 @@ function handlePostState(req, res, responseJSON, theState) {
         switch (theState.inStateId) {
             case 'create':
                 if (formValues.approveDraftConsent === 'yes') {
-                    responseJSON.newRoute = 'notes-for-court-administrator';
+                    responseJSON.newRoute = 'notes-for-court-administrator'
                 } else {
-                    responseJSON.newRoute = 'reject-reasons';
+                    responseJSON.newRoute = 'reject-reasons'
                 }
-                break;
+                break
             case 'notes-for-court-administrator':
-                responseJSON.newRoute = 'check';
-                break;
+                responseJSON.newRoute = 'check'
+                break
             case 'check':
-                responseJSON.newRoute = 'decision-confirmation';
-                break;
+                responseJSON.newRoute = 'decision-confirmation'
+                break
             case 'reject-reasons':
                 if (formValues.includeAnnotatedVersionDraftConsOrder === 'yes') {
-                    responseJSON.newRoute = 'draft-consent-order';
+                    responseJSON.newRoute = 'draft-consent-order'
                 } else if (formValues.partiesNeedAttend === true) {
-                    responseJSON.newRoute = 'hearing-details';
+                    responseJSON.newRoute = 'hearing-details'
                 } else {
-                    responseJSON.newRoute = 'notes-for-court-administrator';
+                    responseJSON.newRoute = 'notes-for-court-administrator'
                 }
-                break;
+                break
             case 'draft-consent-order':
                 if (formValues.partiesNeedAttend === true) {
-                    responseJSON.newRoute = 'hearing-details';
+                    responseJSON.newRoute = 'hearing-details'
                 } else {
-                    responseJSON.newRoute = 'notes-for-court-administrator';
+                    responseJSON.newRoute = 'notes-for-court-administrator'
                 }
-                break;
+                break
             case 'hearing-details':
-                responseJSON.newRoute = 'check';
-                break;
+                responseJSON.newRoute = 'check'
+                break
             default:
-                break;
+                break
         }
         // update meta data according to newly selected state
         if (responseJSON.newRoute) {
-            responseJSON.meta = stateMeta[theState.inJurisdiction][responseJSON.newRoute];
+            responseJSON.meta = stateMeta[theState.inJurisdiction][responseJSON.newRoute]
         }
     }
 
@@ -88,27 +92,27 @@ function handlePostState(req, res, responseJSON, theState) {
 
 function responseAssert(res, responseJSON, inJurisdiction, inStateId, statusHint) {
     if (stateMeta[inJurisdiction] && stateMeta[inJurisdiction][inStateId]) {
-        res.status(ERROR404);
-        responseJSON.statusHint = statusHint;
-        return false;
+        res.status(ERROR404)
+        responseJSON.statusHint = statusHint
+        return false
     }
 
-    return true;
+    return true
 }
 
 async function handleStateRoute(req, res) {
-    const store = new Store(req);
-    const inJurisdiction = req.params.jur_id;
-    const inCaseId = req.params.case_id;
-    const inStateId = req.params.state_id;
+    const store = new Store(req)
+    const inJurisdiction = req.params.jur_id
+    const inCaseId = req.params.case_id
+    const inStateId = req.params.state_id
 
     const theState = {
         inJurisdiction,
         inCaseId,
         inStateId
-    };
+    }
 
-    const responseJSON = {};
+    const responseJSON = {}
 
     if (
         responseAssert(res, responseJSON, stateMeta[inJurisdiction], 'Input parameter route_id: uknown jurisdiction') &&
@@ -116,53 +120,45 @@ async function handleStateRoute(req, res) {
             res,
             responseJSON,
             stateMeta[inJurisdiction][inStateId],
-            'Input parameter route_id wrong: no route with this id inside jurisdiction ${inRouteJur}'
+            `Input parameter route_id wrong: no route with this id inside jurisdiction ${inJurisdiction}`
         )
     ) {
         // for GET we return meta for the state requested by inStateId
         // however, for POST, the meta may get overwritten if the change of state occurs
-        responseJSON.meta = stateMeta[inJurisdiction][inStateId];
+        responseJSON.meta = stateMeta[inJurisdiction][inStateId]
 
         if (req.method === 'POST') {
-            handlePostState(req, res, responseJSON, theState);
+            handlePostState(req, res, responseJSON, theState)
         }
 
-        responseJSON.formValues = store.get(`decisions_${inCaseId}`) || {};
+        responseJSON.formValues = store.get(`decisions_${inCaseId}`) || {}
     }
-    console.log(req.headers['ServiceAuthorization']);
-    console.log('########################');
-    console.log(req.auth);
-    console.log('########################');
-    console.log(theState);
-    console.log('########################');
-    console.log(req.session);
-    console.log('########################');
-    
-    let { token, caseDetails } = await postCaseWithEventToken(
+    console.log(req.headers.ServiceAuthorization)
+    console.log('########################')
+    console.log(req.auth)
+    console.log('########################')
+    console.log(theState)
+    console.log('########################')
+    console.log(store.get(`decisions_${inCaseId}`))
+    console.log('########################')
+
+    const { token, caseDetails } = await getCaseWithEventToken(
         req.auth.userId,
         'DIVORCE',
         'FinancialRemedyMVP2',
         inCaseId,
         'FR_approveApplication',
         getOptions(req)
-    );
+    )
 
-
-
-    // .then(response => {
-    //     console.log(response);
-    // })
-    // .catch(e => {
-    //     console.log('error ');
-    // });
-
-    req.session.save(() => res.send(JSON.stringify(responseJSON)));
+    console.log(caseDetails)
+    req.session.save(() => res.send(JSON.stringify(responseJSON)))
 }
 
 module.exports = app => {
-    const router = express.Router({ mergeParams: true });
-    app.use('/decisions', router);
+    const router = express.Router({ mergeParams: true })
+    app.use('/decisions', router)
 
-    router.get('/state/:jur_id/:case_id/:state_id', handleStateRoute);
-    router.post('/state/:jur_id/:case_id/:state_id', handleStateRoute);
-};
+    router.get('/state/:jur_id/:case_id/:state_id', handleStateRoute)
+    router.post('/state/:jur_id/:case_id/:state_id', handleStateRoute)
+}
