43d47a61bf685f7c21b61854ce7affba93568dbf
Alan McCabe-Jones <alan.work@teamsmog.com>
2018-10-18 09:40:36 +0100

    tidy

--- api/decisions/state.js
+++ api/decisions/state.js
@@ -70,7 +70,7 @@ function perpareCaseForConsentOrder(documentAnnotationId, eventToken, eventId, u
 
         ignore_warning: true
     }
-    console.log('payload', payload)
+
     return payload
 }
 
@@ -171,7 +171,6 @@ function perpareCaseForRefusal(caseData, eventToken, eventId, user, store) {
 async function updateConsentOrder(documentAnnotationId, req, state, store) {
     let payload = {}
     let eventToken = {}
-    let caseDetails = {}
 
     try {
         logger.info('Getting Event Token')
@@ -186,7 +185,6 @@ async function updateConsentOrder(documentAnnotationId, req, state, store) {
         )
 
         eventToken = eventTokenAndCAse.token
-        caseDetails = eventTokenAndCAse.caseDetails
 
         logger.info(`Got token ${eventToken}`)
     } catch (exception) {
