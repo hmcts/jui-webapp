d40a23cb2c7780df6b08325e9da265b7997f0ee2
rramisetty <roopa.ramisetty@hmcts.net>
2018-10-18 17:10:05 +0100

    updated parties page elements n steps

--- test/e2e/features/pages/partiesPage.js
+++ test/e2e/features/pages/partiesPage.js
@@ -1,6 +1,9 @@
 'use strict';
 
 function partiesPage(){
+    this.parties_tabs = element.all(by.css("a.govuk-tabs__tab"));
+    this.petitioner_details = element.all(by.css("tr td[data-selector='table-cell']"));
+
 
 
 
--- test/e2e/features/step_definitions/steps/parties.steps.js
+++ test/e2e/features/step_definitions/steps/parties.steps.js
@@ -10,20 +10,34 @@ defineSupportCode(function({ Given, When, Then }) {
 
 
     Then(/^I can see Petitioner and Respondent tabs$/, async function() {
+        await expect(partiesPage.parties_tabs.first().getText()).to.eventually.equal("Petitioner");
+        await expect(partiesPage.parties_tabs.get(1).getText()).to.eventually.equal("Respondent");
 
 
     });
 
     When(/^I select Petitioner tab$/, async function() {
+        await partiesPage.parties_tabs.first().click();
+        browser.sleep(3000);
 
     });
 
 
     When(/^I select Respondent tab$/, async function() {
 
+        await partiesPage.parties_tabs.get(1).click();
+        browser.sleep(3000);
+
     });
 
     Then(/^I can see (.*), (.*), (.*) , (.*)$/, async function(fullname, Address, phone, email, representative) {
+        await expect(partiesPage.petitioner_details.first().getText()).to.eventually.equal(fullname);
+        await expect(partiesPage.petitioner_details.get(2).getText()).to.eventually.equal(Address);
+        await expect(partiesPage.petitioner_details.get(4).getText()).to.eventually.equal(phone);
+        await expext(partiesPage.petitioner_details.get(6).getText()).to.eventually.equal(email);
+        await expext(partiesPage.petitioner_details.get(8).getText()).to.eventually.equal(representative);
+
+
 
     });
 
