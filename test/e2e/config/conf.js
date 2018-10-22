c23b9418f84c0db4f257913928d3e843d5db840e
Alec Doran-Twyford <alec@alectronic.co>
2018-10-22 20:14:40 +0100

    Update conf.js

--- test/e2e/config/conf.js
+++ test/e2e/config/conf.js
@@ -17,7 +17,7 @@ const jenkinsConfig = [
     {
         browserName: 'chrome',
         acceptInsecureCerts: true,
-        nogui: true
+        nogui: true,
 
         chromeOptions: { args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-zygote ', '--disableChecks' ] }
     }
