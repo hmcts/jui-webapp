hmcts/webappclonejui

adding document to events

api/events/event.js
@@ -3,6 +3,8 @@ const moment = require('moment');
 const config = require('../../config');
 const generateRequest = require('../lib/request');
 const mockRequest = require('../lib/mockRequest');
+const getEventTemplate = require('./templates');
+const valueProcessor = require('../lib/processors/value-processor');
 
 // TODO move [postHearing, getHearingId, getOnlineHearingConversation] to COH microserivce Module
 function postHearing(caseId, userId, options, jurisdictionId = 'SSCS') {
@@ -49,14 +51,24 @@ function convertDateTime(dateObj) {
 /// CCD EVENT Data
 //////////////////////////
 
-function reduceCcdEvents(events) {
+function reduceCcdEvents(events, caseId, jurisdiction, caseType) {
     return events.map(event => {
         const dateObj = convertDateTime(event.created_date);
         const dateUtc = dateObj.dateUtc;
         const date = dateObj.date;
         const time = dateObj.time;
 
-        const documents = [];
+
+
+        valueProcessor(getEventTemplate(jurisdiction, caseType), event);
+
+        const documents = event.documents.map(doc => {
+            return ({
+                name: `${doc.document_filename}`,
+                href: `/jurisdiction/${jurisdiction}/casetype/${caseType}/viewcase/${caseId}/casefile/${doc.id}`
+            });
+        });
+
 
         return {
             title: event.event_name,
@@ -75,7 +87,7 @@ function getCcdEventsRaw(caseId, userId, jurisdiction, caseType, options) {
 }
 
 function getCcdEvents(caseId, userId, jurisdiction, caseType, options) {
-    return getCcdEventsRaw(caseId, userId, jurisdiction, caseType, options).then(reduceCcdEvents);
+    return getCcdEventsRaw(caseId, userId, jurisdiction, caseType, options).then(events => reduceCcdEvents(events, caseId, jurisdiction, caseType));
 }
 
 //////////////////////////

hmcts/jui-webapp

api/events/event.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const moment = require('moment');
+const getEventTemplate = require('./templates');
+const valueProcessor = require('../lib/processors/value-processor');
 
 const { getCCDEvents } = require('../services/ccd-store-api/ccd-store');
 const { getHearingIdOrCreateHearing, getOnlineHearingConversation } = require('../services/coh-cor-api/coh-cor-api');
@@ -25,28 +27,38 @@ function convertDateTime(dateObj) {
 /// CCD EVENT Data
 //////////////////////////
 
-function reduceCcdEvents(events) {
+function reduceCcdEvents(events, caseId, jurisdiction, caseType) {
     return events.map(event => {
         const dateObj = convertDateTime(event.created_date);
         const dateUtc = dateObj.dateUtc;
         const date = dateObj.date;
         const time = dateObj.time;
 
-        const documents = [];
+
+
+        valueProcessor(getEventTemplate(jurisdiction, caseType), event);
+
+        const documents = event.documents.map(doc => {
+            return ({
+                name: `${doc.document_filename}`,
+                href: `/jurisdiction/${jurisdiction}/casetype/${caseType}/viewcase/${caseId}/casefile/${doc.id}`
+            });
+        });
+
 
         return {
             title: event.event_name,
             by: `${event.user_first_name} ${event.user_last_name}`,
             dateUtc,
             date,
-            time,
-            documents
+            time
+            // ,documents // renable when we want to actuall show documents
         };
     });
 }
 
 function getCcdEvents(caseId, userId, jurisdiction, caseType, options) {
-    return getCCDEvents(caseId, userId, jurisdiction, caseType, options).then(reduceCcdEvents);
+    return getCCDEvents(caseId, userId, jurisdiction, caseType, options).then(events => reduceCcdEvents(events, caseId, jurisdiction, caseType));
 }
 
 //////////////////////////