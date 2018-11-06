dcfda38d6c4b94a69a706483e015fa232ab02ebb
Chris Tarasovs <chris@frontr.co.uk>
2018-11-06 15:30:14 +0000

    Refactor: seperating mock data in SummaryPanelComponent

--- src/app/domain/case-viewer/components/summary-panel/summary-panel.component.spec.ts
+++ src/app/domain/case-viewer/components/summary-panel/summary-panel.component.spec.ts
@@ -4,6 +4,7 @@ import { SummaryPanelComponent } from './summary-panel.component';
 import { DebugElement } from '@angular/core';
 import { Selector } from '../../../../../../test/selector-helper';
 import {RouterTestingModule} from '@angular/router/testing';
+import {mockPanelData} from './mock/summary-panel.mock';
 
 describe('SummaryPanelComponent', () => {
     let component: SummaryPanelComponent;
@@ -31,66 +32,7 @@ describe('SummaryPanelComponent', () => {
     describe('Setting inputs', () => {
         beforeEach(() => {
 
-            component.panelData = {
-                'name': 'Summary',
-                'type': 'summary-panel',
-                'sections': [
-                    {
-                        'name': 'Case Details',
-                        'type': 'data-list',
-                        'fields': [
-                            {
-                                'label': 'Parties',
-                                'value': 'A May_146863 v DWP'
-                            },
-                            {
-                                'label': 'Case number',
-                                'value': 'SC001/01/46863'
-                            },
-                        ]
-                    },
-                    {
-                        'name': 'Representative',
-                        'type': 'data-list',
-                        'fields': [
-                            {
-                                'label': 'Judge',
-                                'value': 'na'
-                            },
-                            {
-                                'label': 'Medical member',
-                                'value': 'na'
-                            },
-                            {
-                                'label': 'Disability qualified member',
-                                'value': 'na'
-                            }
-                        ]
-                    },
-                    {
-                        'name': 'Recent events',
-                        'type': 'timeline',
-                        'fields': [
-                            {
-                                'value': [
-                                    {
-                                        'event_name': 'Create/update Panel',
-                                        'user_first_name': 'John',
-                                        'user_last_name': 'Smith',
-                                        'created_date': '2018-07-05T11:37:44.854'
-                                    },
-                                    {
-                                        'event_name': 'Appeal created',
-                                        'user_first_name': 'Gilbert',
-                                        'user_last_name': 'Smith',
-                                        'created_date': '2018-07-05T11:36:51.125'
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            };
+            component.panelData = mockPanelData;
 
             fixture.detectChanges();
         });
