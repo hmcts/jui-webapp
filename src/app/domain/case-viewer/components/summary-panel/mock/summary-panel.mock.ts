b2746f23b562df917c68afa286e6d12fab800af8
Chris Tarasovs <chris@frontr.co.uk>
2018-11-06 16:08:37 +0000

    Created an Interface for SummaryPanelComponent

--- src/app/domain/case-viewer/components/summary-panel/mock/summary-panel.mock.ts
+++ src/app/domain/case-viewer/components/summary-panel/mock/summary-panel.mock.ts
@@ -1,32 +1,31 @@
-export interface Sections {
+export interface PageDate {
+    name: string;
+    type: string;
     sections: Array<SectionsItem>;
-}
-export interface FieldItem{
-    
 }
 export interface SectionsItem {
     name: string;
     type: string;
-    fields: Array<>;
-    'fields': [
+    fields: Array<FieldItem>;
+}
+export interface FieldItem {
+    label?: string;
+    value: string | Array<FieldItemValue>;
+}
+export interface FieldItemValue {
+    event_name: string;
+    user_first_name: string;
+    user_last_name: string;
+    created_date: string;
+}
+export const mockPanelData: PageDate = {
+    name: 'Summary',
+    type: 'summary-panel',
+    sections: [
         {
-            'label': 'Parties',
-            'value': 'A May_146863 v DWP'
-        },
-        {
-            'label': 'Case number',
-            'value': 'SC001/01/46863'
-        },
-        ]
-},
-export const mockPanelData = {
-    'name': 'Summary',
-    'type': 'summary-panel',
-    'sections': [
-        {
-            'name': 'Case Details',
-            'type': 'data-list',
-            'fields': [
+            name: 'Case Details',
+            type: 'data-list',
+            fields: [
                 {
                     'label': 'Parties',
                     'value': 'A May_146863 v DWP'
@@ -38,9 +37,9 @@ export const mockPanelData = {
             ]
         },
         {
-            'name': 'Representative',
-            'type': 'data-list',
-            'fields': [
+            name: 'Representative',
+            type: 'data-list',
+            fields: [
                 {
                     'label': 'Judge',
                     'value': 'na'
@@ -56,22 +55,22 @@ export const mockPanelData = {
             ]
         },
         {
-            'name': 'Recent events',
-            'type': 'timeline',
-            'fields': [
+            name: 'Recent events',
+            type: 'timeline',
+            fields: [
                 {
-                    'value': [
+                    value: [
                         {
-                            'event_name': 'Create/update Panel',
-                            'user_first_name': 'John',
-                            'user_last_name': 'Smith',
-                            'created_date': '2018-07-05T11:37:44.854'
+                            event_name: 'Create/update Panel',
+                            user_first_name: 'John',
+                            user_last_name: 'Smith',
+                            created_date: '2018-07-05T11:37:44.854'
                         },
                         {
-                            'event_name': 'Appeal created',
-                            'user_first_name': 'Gilbert',
-                            'user_last_name': 'Smith',
-                            'created_date': '2018-07-05T11:36:51.125'
+                            event_name: 'Appeal created',
+                            user_first_name: 'Gilbert',
+                            user_last_name: 'Smith',
+                            created_date: '2018-07-05T11:36:51.125'
                         }
                     ]
                 }
