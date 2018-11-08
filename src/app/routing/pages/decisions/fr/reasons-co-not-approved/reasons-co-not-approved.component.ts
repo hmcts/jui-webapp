fed6426b06ff1eb970b079f6292470dfd34ab915
Dmitry Panov <dpanov76@yahoo.com>
2018-11-08 16:52:18 +0000

    Form builder-fixes

--- src/app/routing/pages/decisions/fr/reasons-co-not-approved/reasons-co-not-approved.component.ts
+++ src/app/routing/pages/decisions/fr/reasons-co-not-approved/reasons-co-not-approved.component.ts
@@ -1,6 +1,6 @@
 import { Component, Attribute, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import {FormGroup, ValidationErrors, ValidatorFn} from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { DecisionService } from '../../../../../domain/services/decision.service';
 import { FormsService } from '../../../../../shared/services/forms.service';
 import { ValidationService } from '../../../../../shared/services/validation.service';
@@ -20,10 +20,17 @@ export class ReasonsCoNotApprovedComponent implements OnInit {
     request: any;
     pageValues: any = null;
     case: any;
-    showOther: boolean = false;
-    showOther2: boolean = false;
-    showChilds: FormGroup;
     pageitems;
+
+    /**
+     * showValidation
+     *
+     * As per the prototype, initially we do not show validation, only when the user clicks on 'Continue' should we
+     * show the validation issues, for each control.
+     *
+     * @type {boolean}
+     */
+    useValidation = false;
 
     constructor( @Attribute('data-children-of') private type: string,
                  private activatedRoute: ActivatedRoute,
@@ -38,6 +45,38 @@ export class ReasonsCoNotApprovedComponent implements OnInit {
          * Creates Form Group with Controls.
          */
         this.rejectReasonsForm = new FormGroup(this.formsService.defineformControls(pageitems, pageValues));
+
+        const checkboxes: Array<string> = ['partiesNeedAttend', 'NotEnoughInformation', 'orderNotAppearOfS25ca1973', 'd81',
+            'pensionAnnex', 'applicantTakenAdvice', 'respondentTakenAdvice', 'Other2'];
+
+        const validationIdentifier = 'reasonsConstentOrderNotApproved';
+
+        /**
+         * Form Group Validators, are used for validation that involves one control, being dependent upon another,
+         * or on a group of other controls.
+         *
+         * Validation is required on the common ancestor as per
+         * @see https://angular.io/guide/form-validation#adding-to-reactive-forms-1
+         * to validate multiply controls.
+         *
+         * TODO: So over here, you can place in multiply validators for the page.
+         * So over here you need to pass in the checkboxes. how would you do this?
+         *
+         * Therefore isAnyCheckboxChecked needs to return an object that is of signature of ValidationFn.
+         *
+         * So a dev would add validators here, each validator, would have a name
+         * So you should be able to pass in.
+         *
+         */
+        const formGroupValidators = [this.validationService.isAnyCheckboxChecked(this.rejectReasonsForm, checkboxes, validationIdentifier)];
+
+        /**
+         * Sets up Forms top most validators, ie. validators that depend on multiply controls. ie.
+         * to check if one of multiply checkboxes are checked.
+         *
+         * Note : set Validations takes objects with a signature of ValidationFn
+         */
+        this.rejectReasonsForm.setValidators(formGroupValidators);
     }
     ngOnInit() {
         this.rejectReasonsForm = null;
