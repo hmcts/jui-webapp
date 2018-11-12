ff4417576b73eefad767783a4a6be7522d451ba3
rramisetty <roopa.ramisetty@hmcts.net>
2018-11-12 15:23:21 +0000

    added approve decision steps

--- test/e2e/features/app/makeDecision.feature
+++ test/e2e/features/app/makeDecision.feature
@@ -3,16 +3,17 @@ Feature: Make Decision
 
    Background:
        Given I navigate to JUI Url
-        Then I am logged into JUI web app
+       Then I am logged into JUI web app with FR judge details
         When I select a case reference
-        Then I click the Make decision button
+       Then I click on Make decision button
 
     @makedecision @all @smoke
     Scenario: Verify the Don't Approve on Make Decision
         Then I select Don't Approve and click continue
         Then I verify the Check Decision page
 
-        @makedecision @all
+    @makedecision @all @RIUI_1117
+        # Note:Here im not adding Approve decision test as each approved once will delete from dashboard
         Scenario: Verify Approving a Make decision
             Then I see approve the draft consent order option
             When I click on "Yes" option
--- test/e2e/features/step_definitions/steps/makeDecision.steps.js
+++ test/e2e/features/step_definitions/steps/makeDecision.steps.js
@@ -8,28 +8,12 @@ const EC = protractor.ExpectedConditions;
 defineSupportCode(function ({Given, When, Then}) {
 
 
-    Then(/^click on Back to Dashboard takes me to Dashboard Page$/, async function () {
+    Then(/^I click on Make decision button$/, async function () {
         browser.sleep(MID_DELAY);
-        await timelinePage.back_to_dashboard_link.click();
-        await expect(timelinePage.back_to_dashboard.isDisplayed()).to.eventually.be.true;
+         await browser.wait(EC.elementToBeClickable(timelinePage.make_decision.first()), MID_DELAY);
+         await timelinePage.make_decision.first().click();
     });
 
-    When(/^I submit the Decision should show Decision confirmation$/, async function () {
-        browser.sleep(MID_DELAY);
-        await timelinePage.button_continue.click();
-        browser.sleep(MID_DELAY);
-        await timelinePage.decision_confirmed.getText().then(function (text) {
-            console.log(text);
-            expect(text).equal('Decision confirmed');
-        });
-    });
-
-    Then(/^I click the Make Decision button$/,  async function () {
-        browser.sleep(MID_DELAY);
-        var loginButton = timelinePage.make_decision;
-        await browser.wait(EC.elementToBeClickable(loginButton), MID_DELAY);
-        await loginButton.click();
-    });
 
     Then(/^I select Don't Approve and click continue$/, async function () {
         browser.sleep(MID_DELAY);
@@ -56,35 +40,43 @@ defineSupportCode(function ({Given, When, Then}) {
 
 
     Then(/^I see approve the draft consent order option$/, async function () {
+        browser.sleep(MID_DELAY);
        await expect(makeDecisionPage.draft_consent_order_text.isDisplayed()).to.be.eventually.be.true;
+        browser.sleep(MID_DELAY);
        await expect(makeDecisionPage.draft_consent_order_text.getText()).to.be.eventually.equal('Do you want to approve the draft consent order?');
 
     });
 
 
     When(/^I click on "Yes" option$/, async function () {
+        browser.sleep(SHORT_DELAY);
         await makeDecisionPage.yes_btn.click();
 
 
     });
 
     Then(/^ I click on Continue$/, async function () {
+        browser.sleep(SHORT_DELAY);
         await makeDecisionPage.continue_btn.click();
 
 
     });
 
     Then(/^I enter notes for court administration$/, async function () {
-        await makeDecisionPage.textarea.send_keys('test draft consent order');
+        browser.sleep(SHORT_DELAY);
+        await makeDecisionPage.textarea.clear();
+        await makeDecisionPage.textarea.sendKeys('test draft consent order');
 
     });
 
     When(/^I click on Continue$/, async function (){
+        browser.sleep(SHORT_DELAY);
     await makeDecisionPage.continue_btn.click();
 
     });
 
     Then(/^I am on check your decision page$/, async function () {
+        browser.sleep(SHORT_DELAY);
         await expect(makeDecisionPage.check_decision_header.isDisplayed()).to.be.eventually.be.true;
         await expect(makeDecisionPage.check_decision_header.getText()).to.be.eventually.equal('Check your decision');
 
