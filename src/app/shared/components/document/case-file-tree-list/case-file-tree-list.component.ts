2403a929ae776d63a81d5f35c00a67720e41f5f8
ficklephil <philip.aberneithy@gmail.com>
2018-12-10 17:03:28 +0000

    Pull through the document modified date to show to the user.

--- src/app/shared/components/document/case-file-tree-list/case-file-tree-list.component.ts
+++ src/app/shared/components/document/case-file-tree-list/case-file-tree-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import moment from 'moment';
 
 @Component({
     selector: 'app-case-file-tree-list',
@@ -9,9 +10,21 @@ export class CaseFileTreeListComponent implements OnInit {
 
     @Input() documents: any;
 
-    constructor() { }
+    constructor() {
+    }
 
     ngOnInit() {
+    }
+
+    /**
+     * getHumanReadableDate
+     *
+     * @param isoDate - 2018-11-21T10:39:02+0000
+     *
+     * @return {string} - 21st Nov 2018, 10:39:02 am
+     */
+    getHumanReadableDate(isoDate) {
+        return moment(isoDate).format('Do MMM YYYY, h:mm:ss a');
     }
 
 }
