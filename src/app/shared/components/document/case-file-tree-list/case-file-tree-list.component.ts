1ce7bf58467a8cd12ef9937ecdf2356205527df1
ficklephil <philip.aberneithy@gmail.com>
2018-12-11 10:30:54 +0000

    Using angular date over moment.

--- src/app/shared/components/document/case-file-tree-list/case-file-tree-list.component.ts
+++ src/app/shared/components/document/case-file-tree-list/case-file-tree-list.component.ts
@@ -1,5 +1,4 @@
 import {Component, Input, OnInit} from '@angular/core';
-import moment from 'moment';
 
 @Component({
     selector: 'app-case-file-tree-list',
@@ -15,16 +14,4 @@ export class CaseFileTreeListComponent implements OnInit {
 
     ngOnInit() {
     }
-
-    /**
-     * getHumanReadableDate
-     *
-     * @param isoDate - 2018-11-21T10:39:02+0000
-     *
-     * @return {string} - 21st Nov 2018, 10:39:02 am
-     */
-    getHumanReadableDate(isoDate) {
-        return moment(isoDate).format('Do MMM YYYY, h:mm:ss a');
-    }
-
 }
