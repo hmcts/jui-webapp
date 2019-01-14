98e18b263efc326198908baf3f3fff4a6432f509
Toireasa Moley <toireasa.moley@hmcts.net>
2019-01-14 14:14:19 +0000

    Removing health check

--- config/environments/demo.config.ts
+++ config/environments/demo.config.ts
@@ -5,7 +5,7 @@ export default {
         ccd_def_api:
             'https://ccd-definition-store-api-aat.service.core-compute-aat.internal',
         idam_web: 'https://idam.preprod.ccidam.reform.hmcts.net',
-        idam_api: 'https://preprod-idamapi.reform.hmcts.net:3511',
+        //idam_api: 'https://preprod-idamapi.reform.hmcts.net:3511',
         s2s:
             'https://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
         draft_store_api:
