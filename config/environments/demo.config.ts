30d2cb653e63fa976ae97ee0f38337e4704ddb01
Pardeep Basi <Pardeep.Basi@hmcts.net>
2019-01-16 12:42:47 +0000

    enabled idam_api as we can't authenticate otherwise. Also enabled secure cookie session for demo and prod environments

--- config/environments/demo.config.ts
+++ config/environments/demo.config.ts
@@ -5,7 +5,7 @@ export default {
         ccd_def_api:
             'https://ccd-definition-store-api-aat.service.core-compute-aat.internal',
         idam_web: 'https://idam.preprod.ccidam.reform.hmcts.net',
-        //idam_api: 'https://preprod-idamapi.reform.hmcts.net:3511',
+        idam_api: 'https://preprod-idamapi.reform.hmcts.net:3511',
         s2s:
             'https://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
         draft_store_api:
@@ -17,6 +17,6 @@ export default {
         coh_cor_api: 'https://coh-cor-demo.service.core-compute-demo.internal'
     },
     useProxy: false,
-    secureCookie: false,
+    secureCookie: true,
     sessionSecret: 'secretSauce'
 };
