1a109cd987938ada9146da2941b7c93ed280c812
Pardeep Basi <Pardeep.Basi@hmcts.net>
2019-01-17 12:08:29 +0000

    wrong quotes on CSP meant blocked resources, fixed

--- application.ts
+++ application.ts
@@ -22,12 +22,12 @@ const appInsightsInstrumentationKey =
 app.use(helmet());
 app.use(helmet.contentSecurityPolicy({
     directives: {
-        fontSrc: ['"self"', 'data:'],
-        scriptSrc: ['"self"', '"unsafe-inline"'],
-        connectSrc: ['"self"'],
-        mediaSrc: ['"self"'],
-        frameSrc: ['"none"'],
-        imgSrc: ['"self"']
+        fontSrc: [`'self'`, 'data:'],
+        scriptSrc: [`'self'`, `'unsafe-inline'`],
+        connectSrc: [`'self'`],
+        mediaSrc: [`'self'`],
+        frameSrc: [`'self'`],
+        imgSrc: [`'self'`]
     }
 }));
 app.use(helmet.referrerPolicy({ policy: 'origin' }));
