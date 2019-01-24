ccca8b4e28e7d2183f402b269947067e528a96f9
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2019-01-24 15:08:32 +0000

    stack stackEmpty unit tests added

--- api/lib/stack.spec.ts
+++ api/lib/stack.spec.ts
@@ -8,25 +8,45 @@ import {mockReq, mockRes} from 'sinon-express-mock'
 chai.use(sinonChai)
 import {Store} from './store/store'
 
-import {pushStack} from './stack'
+import {pushStack, stackEmpty} from './stack'
+
+const req = {
+    params: {
+        'caseId': '321',
+        'caseTypeId': '3',
+        'jurId': '123',
+    },
+    session: {
+        'foo': 'bar',
+    },
+}
 
 describe('stack', () => {
     describe('pushStack', () => {
         it('Should construct value to push to store', async () => {
-            const req = {
-                params: {
-                    'caseId': '321',
-                    'caseTypeId': '3',
-                    'jurId': '123',
-                },
-                session: {
-                    'foo': 'bar',
-                },
-            }
             const stack = [1, 2, 3]
-            sinon.spy(Store.prototype, 'set')
+            const spy = sinon.spy(Store.prototype, 'set')
             await pushStack(req, stack)
-            expect(Store.prototype.set).to.be.calledWith('decisions_stack_123_3_321', [1, 2, 3])
+            expect(spy).to.be.calledWith('decisions_stack_123_3_321', [1, 2, 3])
+            spy.restore()
+        })
+    })
+    describe('stackEmpty', () => {
+        it('Should return false if stack is not empty', async () => {
+            const stub = sinon.stub(Store.prototype, 'get')
+            stub.returns('decisions_stack_123_3_321')
+            const result = await stackEmpty(req)
+            expect(result).to.equal(false)
+            stub.restore()
+
+        })
+        it('Should return if stack is empty', async () => {
+            const stub = sinon.stub(Store.prototype, 'get')
+            stub.returns(false)
+            const result = await stackEmpty(req)
+            expect(result).to.equal(true)
+            stub.restore()
+
         })
     })
 })
