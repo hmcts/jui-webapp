28a9d42c1d1a4fde4678d92a7bcb6995ba7d82e9
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2019-01-25 14:05:34 +0000

    Added interceptors test - still need to finish one function

---
+++ api/lib/interceptors.spec.ts
@@ -0,0 +1,75 @@
+import * as chai from 'chai'
+import {expect} from 'chai'
+import * as log4js from 'log4js'
+import 'mocha'
+import * as sinon from 'sinon'
+import * as sinonChai from 'sinon-chai'
+import {mockReq, mockRes} from 'sinon-express-mock'
+
+chai.use(sinonChai)
+
+import {errorInterceptor, requestInterceptor, successInterceptor} from './interceptors'
+
+describe('interceptors', () => {
+    const response = {
+        config: {
+            method: 'POST',
+            url: 'http://test2.com',
+        },
+    }
+    const request = {
+        method: 'GET',
+        url: 'http://test.com',
+    }
+    const error = {
+        config: {
+            data: {
+                authenticated: false,
+            },
+            method: 'GET',
+            response: {
+                status: 500,
+            },
+            url: 'http://test.com',
+        },
+    }
+    describe('requestInterceptor', () => {
+        it('Should log outbound request', () => {
+            const spy = sinon.spy()
+            const getLoggerStub = sinon.stub(log4js, 'getLogger')
+            getLoggerStub.returns({info: spy})
+            requestInterceptor(request)
+            expect(spy).to.be.calledWith('GET to http://test.com')
+            getLoggerStub.restore()
+        })
+        it('Should return request unmutilated', () => {
+            const result = requestInterceptor(request)
+            expect(result).to.be.equal(request)
+        })
+    })
+    describe('successInterceptor', () => {
+        it('Should log returned response', () => {
+            const spy = sinon.spy()
+            const getLoggerStub = sinon.stub(log4js, 'getLogger')
+            getLoggerStub.returns({info: spy})
+            successInterceptor(response)
+            expect(spy).to.be.calledWith('Success on POST to http://test2.com')
+            getLoggerStub.restore()
+        })
+        it('Should return response unmutilated', () => {
+            const result = successInterceptor(response)
+            expect(result).to.be.equal(response)
+        })
+    })
+    describe('errorInterceptor', () => {
+        // it('Should log returned error', () => {
+        //     const spy = sinon.spy()
+        //     const getLoggerStub = sinon.stub(log4js, 'getLogger')
+        //     // stub.returns('ABC')
+        //     getLoggerStub.returns({info: spy})
+        //     errorInterceptor(error)
+        //     expect(spy).to.be.calledWith('Success on POST to http://test2.com')
+        //     getLoggerStub.restore()
+        // })
+    })
+})
