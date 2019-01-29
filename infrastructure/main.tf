f361d3231a675ba18ce7a071cf7eda07726e12fa
Alan McCabe - Jones <alan@localhost.localdomain>
2019-01-29 15:48:08 +0000

    jenkins secrets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -56,8 +56,8 @@ data "azurerm_key_vault" "key_vault" {
     resource_group_name = "${local.shared_vault_name}"
 }
 
-data "azurerm_key_vault_secret" "decryptKey" {
-   name = "decrypt-Key"
+data "azurerm_key_vault_secret" "decrypt-key" {
+   name = "decrypt-key"
    vault_uri = "${data.azurerm_key_vault.key_vault.vault_uri}"
 }
 
