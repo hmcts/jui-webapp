36f602476f7c374eebab2d3bd38f82c79024e75a
ficklephil <philip.aberneithy@gmail.com>
2019-02-04 11:40:03 +0000

    set type as string, like it was previously, to fix ng unit test.

--- src/app/shared/components/case-actions/case-actions.component.ts
+++ src/app/shared/components/case-actions/case-actions.component.ts
@@ -7,7 +7,7 @@ import {LinkItem} from '../../../domain/models/section_fields';
 })
 export class CaseActionsComponent {
 
-    @Input() header;
+    @Input() header: string;
     @Input() actionPrimaryButton: LinkItem;
     @Input() actionSecondaryButton: LinkItem;
     @Input() actionThirdButton: LinkItem;
