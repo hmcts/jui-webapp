b0fb3bc397240cecce76154ab18c79df6ec9855d
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2019-02-07 09:56:09 +0000

    Tests added to 'api/controllers/decisions/'

--- api/controllers/decisions/states.spec.ts
+++ api/controllers/decisions/states.spec.ts
@@ -4,8 +4,6 @@ import 'mocha'
 import * as sinon from 'sinon'
 import * as sinonChai from 'sinon-chai'
 import {mockReq, mockRes} from 'sinon-express-mock'
-import * as request from 'supertest'
-import * as app from '../../../dev-server'
 import * as state from '../../lib/stateEngine'
 import * as sscs from './sscs'
 
@@ -14,13 +12,6 @@ chai.use(sinonChai)
 import * as states from './states'
 
 describe('states', () => {
-    describe('default export', () => {
-        // it('Should respond with 200 status', done => {
-        //     request(app)
-        //         .get('/state/123/divorce/322/1')
-        //         .expect(404, done)
-        // })
-    })
     describe('handleStateRoute', () => {
         it('Should call process with divorce mapping if jurId === \'DIVORCE\'', async () => {
             const req = {
