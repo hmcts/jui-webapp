ccfc63a6605f473124ee2acf4e5120043aea5e16
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2019-02-15 09:48:07 +0000

    Further coverage added to index

--- api/controllers/case/index.spec.ts
+++ api/controllers/case/index.spec.ts
@@ -1,5 +1,5 @@
 import * as chai from 'chai'
-import { expect } from 'chai'
+import {expect} from 'chai'
 import 'mocha'
 import * as sinon from 'sinon'
 import * as sinonChai from 'sinon-chai'
@@ -10,7 +10,8 @@ import * as ccdStore from '../../services/ccd-store-api/ccd-store'
 import * as cohCorApi from '../../services/coh-cor-api/coh-cor-api'
 import * as events from '../events'
 import * as questions from '../questions/index'
-import { getCaseWithEventsAndQuestions } from './index'
+import * as utils from '../../lib/util'
+import {getCaseWithEventsAndQuestions} from './index'
 
 describe('index', () => {
     describe('getCaseWithEventsAndQuestions', () => {
@@ -24,13 +25,25 @@ describe('index', () => {
             const stub2 = sandbox.stub(events, 'getEvents')
             const stub3 = sandbox.stub(cohCorApi, 'getHearingByCase')
             const stub4 = sandbox.stub(questions, 'getAllQuestionsByCase')
-            stub.resolves(1)
+            const stub5 = sandbox.stub(utils, 'judgeLookUp')
+            stub.resolves({case_data: {assignedToJudge: true, assignedToJudgeName: 'Judge Smith'}})
             stub2.resolves(2)
             stub3.resolves(3)
             stub4.resolves(4)
+            stub5.returns(5)
             const result = await getCaseWithEventsAndQuestions(userId, jurisdiction, caseType, caseId)
             expect(result).to.be.an('array')
-            expect(result).to.eql([1, 2, 3, 4])
+            expect(result).to.eql([
+                {
+                    'case_data': {
+                        'assignedToJudge': true,
+                        'assignedToJudgeName': 5,
+                    },
+                },
+                2,
+                3,
+                4,
+            ])
             sandbox.restore()
         })
     })
