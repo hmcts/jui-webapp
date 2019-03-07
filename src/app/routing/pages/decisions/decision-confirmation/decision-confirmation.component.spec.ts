7c8b1754ead8e6c7ed83edaa74b1ace45184b4aa
Toireasa Moley <toireasa.moley@hmcts.net>
2019-03-07 14:24:44 +0000

    Testing sonar changes

--- src/app/routing/pages/decisions/decision-confirmation/decision-confirmation.component.spec.ts
+++ src/app/routing/pages/decisions/decision-confirmation/decision-confirmation.component.spec.ts
@@ -7,7 +7,7 @@ import {DecisionService} from '../../../../domain/services/decision.service';
 import {BrowserTransferStateModule} from '@angular/platform-browser';
 import {ConfigService} from '../../../../config.service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
 import {JUIFormsModule} from '../../../../forms/forms.module';
 import {GovukModule} from '../../../../govuk/govuk.module';
@@ -17,8 +17,92 @@ import {CaseService} from '../../../../domain/services/case.service';
 describe('DecisionConfirmationComponent', () => {
     let component: DecisionConfirmationComponent;
     let fixture: ComponentFixture<DecisionConfirmationComponent>;
+    let decisionServiceFetchSpy;
+    let decision;
 
     beforeEach(async(() => {
+        decision{};
+        TestBed.configureTestingModule({
+            declarations: [
+                DecisionConfirmationComponent
+            ],
+            imports: [
+                JUIFormsModule,
+                DomainModule,
+                SharedModule,
+                BrowserTransferStateModule,
+                HttpClientTestingModule,
+                RouterTestingModule,
+                GovukModule,
+                HmctsModule
+            ],
+            providers: [
+                CaseService,
+                DecisionService,
+                {
+                    provide: ConfigService, useValue: {
+                        config: {
+                            api_base_url: ''
+                        }
+                    }
+                },
+                {
+                    provide: ActivatedRoute,
+                    useValue: ({
+                      snapshot: {
+                          _lastPathIndex: 0,
+                          url: [{
+                              path: 'dummy'
+                          }],
+                          parent: {
+                              data: {
+                                  caseData: {
+                                      id: '1234',
+                                      decision: {
+                                          options: [
+                                              { id: 'test', name: 'test' }
+                                          ]
+                                      }
+                                  }
+                              }
+                          }
+                      },
+                      parent: {
+                          data: of({
+                              caseData: {
+                                  id: '1234',
+                                  decision: {
+                                      options: [
+                                          { id: 'test', name: 'test' }
+                                      ]
+                                  }
+                              }
+                          })
+                      }
+                    } as any) as ActivatedRoute
+          
+                  }
+                ]
+              })
+              .compileComponents();
+            }));
+
+            beforeEach(() => {
+                decisionServiceFetchSpy = spyOn(
+                    TestBed.get(DecisionService),
+                    'fetch'
+                ).and.returnValue(of({
+                    meta: {
+                    },
+                    formValues: {}
+                }));
+                fixture = TestBed.createComponent(DecisionConfirmationComponent);
+                component = fixture.componentInstance;
+                fixture.detectChanges();
+              });
+            });
+
+/*    beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
                 DecisionConfirmationComponent
@@ -78,4 +162,4 @@ describe('DecisionConfirmationComponent', () => {
     xit('should create', () => {
         expect(component).toBeTruthy();
     });
-});
+});*/
