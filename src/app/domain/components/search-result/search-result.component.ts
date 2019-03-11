4d37644a5dd83a0c7840871e2055070ed2a7227a
Philip <philip.aberneithy@acp.aws.accenture.com>
2019-03-11 15:37:52 +0000

    Passing back an error from the Observable to hook into.

--- src/app/domain/components/search-result/search-result.component.ts
+++ src/app/domain/components/search-result/search-result.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {CaseService} from '../../services/case.service';
 import {RedirectionService} from '../../../routing/redirection.service';
 import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs';
 
 @Component({
     selector: 'app-search-result',
@@ -17,6 +18,24 @@ export class SearchResultComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.data$ = this.caseService.search();
+
+        const cases = this.caseService.getCases();
+        console.log('cases');
+        console.log(cases);
+        //This is an observable
+        this.data$ = cases;
+
+        // We want to handle the error condition here.
+        // casesObservable.subscribe(cases => {
+        //         console.log('in subscribe');
+        //         console.log(cases);
+        //         this.data$ = cases;
+        //     },
+        //     error => {
+        //         console.log('error condition');
+        //         console.log(error);
+        //     }
+        // );
+
     }
 }
