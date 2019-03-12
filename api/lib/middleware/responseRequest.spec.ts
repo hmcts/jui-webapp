29e73fb3b19ba2d5498220be5699be1e20bb9425
Alan McCabe - Jones <alan@localhost.localdomain>
2019-03-12 16:14:25 +0000

    tests for response request

--- api/lib/middleware/responseRequest.spec.ts
+++ api/lib/middleware/responseRequest.spec.ts
@@ -13,11 +13,38 @@ import * as responseRequest from './responseRequest'
 describe('responseRequest', () => {
     describe('setReqRes', () => {
         it('Should set the request and response objects', () => {
-            const req = mockReq({})
+            const req = mockReq({
+                cookies: [],
+                session: {
+                    user: {
+                        id: 'testId',
+                    },
+                },
+            })
             const res = mockRes()
 
             responseRequest.default(req, res, () => { })
-            expect(responseRequest.response()).to.not.equal(null))
+            expect(responseRequest.response()).to.not.equal(null)
+            expect(responseRequest.request()).to.not.equal(null)
+        })
     })
-})
+
+    describe('request', () => {
+        it('Should return a request object', () => {
+            expect(responseRequest.request()).to.not.equal(null)
+        })
+    })
+
+    describe('response', () => {
+        it('Should return a response object', () => {
+            expect(responseRequest.response()).to.not.equal(null)
+        })
+    })
+
+    describe('isReqResSet', () => {
+        it('Should return true if request and response objects are set', () => {
+            const res = mockRes()
+            expect(responseRequest.isReqResSet()).to.equal(true)
+        })
+    })
 })
