8ad5b60163203d61136dfdc3bfdde977aee19782
Alan McCabe - Jones <alan@localhost.localdomain>
2019-03-14 15:37:32 +0000

    utility

--- api/lib/middleware/auth.spec.ts
+++ api/lib/middleware/auth.spec.ts
@@ -5,9 +5,14 @@ import * as sinon from 'sinon'
 import * as sinonChai from 'sinon-chai'
 import { mockReq, mockRes } from 'sinon-express-mock'
 
+import * as jwt from 'jsonwebtoken'
+
 chai.use(sinonChai)
 
+import { logout } from '../../controllers/auth'
+import * as idam from '../../services/idam'
 import * as auth from './auth'
+
 import * as responseRequest from './responseRequest'
 
 describe('auth', () => {
@@ -18,6 +23,69 @@ describe('auth', () => {
         })
         it('should return false if given roles do not match jui pannel member or judge roles', () => {
             expect(auth.validRoles(['test'])).to.equal(false)
+        })
+    })
+
+    describe('default', () => {
+        it('should call user details if session  does not contain user details', async () => {
+
+            const req = mockReq({
+                cookies: [],
+                headers: [],
+
+            })
+
+            const res = mockRes()
+
+            // lets set the expiry to be tomorrow
+            const expiry = new Date();
+            expiry.setDate(expiry.getDate() + 1)
+            // this should be able to decoded by jwtdecode
+            const token = jwt.sign({ exp: expiry.getTime() }, 'test')
+            req.headers.authorization = token
+            const stub = sinon.stub(idam, 'getDetails')
+            const stub2 = sinon.stub(auth, 'validRoles')
+
+            stub.returns(Promise.resolve({
+                roles: []
+            }))
+
+            stub2.returns(true)
+
+            await auth.default(req, res, () => { })
+            expect(stub).to.be.called
+            stub.restore()
+
+        })
+        it('should log the user out if token has expired', async () => {
+
+            const req = mockReq({
+                cookies: [],
+                headers: [],
+
+            })
+
+            const res = mockRes()
+
+            // lets set the expiry to be tomorrow
+            const expiry = new Date();
+            expiry.setDate(expiry.getDate() - 1)
+            // this should be able to decoded by jwtdecode
+            const token = jwt.sign({ exp: expiry.getTime() }, 'test')
+            req.headers.authorization = token
+            const stub = sinon.stub(auth, 'getDetails')
+            const stub2 = sinon.stub(auth, 'validRoles')
+
+            stub.returns(Promise.resolve({
+                roles: []
+            }))
+
+            stub2.returns(true)
+
+            await auth.default(req, res, () => { })
+            expect(stub).to.be.called
+            stub.restore()
+            stub2.restore()
         })
     })
 })
