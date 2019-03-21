8aa478e6397501af45e082d5057b774b6c2e9c9e
Dmitry Panov <dpanov76@yahoo.com>
2019-03-21 14:32:58 +0000

    fixed code duplication

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,6 +5,10 @@ import {ConfigService} from './config.service';
 import {configMock} from './domain/services/mock/config.env.mock';
 import {RouterTestingModule} from '@angular/router/testing';
 describe('AppComponent', () => {
+
+    let app;
+    let fixture;
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [RouterTestingModule],
@@ -19,26 +23,23 @@ describe('AppComponent', () => {
             ],
             schemas:[CUSTOM_ELEMENTS_SCHEMA]
         }).compileComponents();
+
+        fixture = TestBed.createComponent(AppComponent);
+        app = fixture.debugElement.componentInstance;
+
     }));
     it('should create the app', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
         const component = fixture.componentInstance;
         expect(component).toBeTruthy();
         expect(component).not.toBeNull();
     }));
     it(`should have as title 'app'`, async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
         expect(app.title).toEqual('JUI Web App');
     }));
     it(`should replace title`, async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
         expect(['test', '/']).toContain(app.replacedTitles('test'));
     }));
     it(`should return title mapping `, async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
         expect('Your cases - Judicial case manager').toContain(app.getTitle('/'));
     }));
 });
