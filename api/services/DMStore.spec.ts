f92182aa674bf439114a9c58589829c5246ac342
Philip <philip.aberneithy@gmail.com>
2019-03-21 16:36:56 +0000

    Working through DM store unit tests.

--- api/services/DMStore.spec.ts
+++ api/services/DMStore.spec.ts
@@ -1,138 +1,132 @@
-import * as chai from 'chai'
-import { expect } from 'chai'
-import 'mocha'
-import * as sinon from 'sinon'
-import * as sinonChai from 'sinon-chai'
-import { mockReq, mockRes } from 'sinon-express-mock'
-chai.use(sinonChai)
+import * as chai from 'chai';
+import {expect} from 'chai';
+import 'mocha';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+import {mockReq, mockRes} from 'sinon-express-mock';
 
-import { http } from '../lib/http'
-import * as DMStore from './DMStore'
+chai.use(sinonChai);
 
-import { config } from '../../config'
+import {http} from '../lib/http';
+import * as DMStore from './DMStore';
+
+import {config} from '../../config';
 
 describe('DMStore', () => {
     const res = {
         data: 'okay',
-    }
+    };
 
     const url = config.services.dm_store_api
 
-    // let spy: any
+    let spy: any
+    let spyResolvesAsNull: any
+
     let spyDelete: any
     let spyPost: any
     let spyPut: any
+    let spyPatch: any
 
     beforeEach(() => {
-        spyDelete = sinon.stub(http, 'delete').callsFake(() => {
-            return Promise.resolve(res)
-        })
 
-        spyPost = sinon.stub(http, 'post').callsFake(() => {
-            return Promise.resolve(res)
-        })
+        spy = sinon.stub(http, 'get').resolves(res)
+        spyPost = sinon.stub(http, 'post').resolves(res)
+        spyPatch = sinon.stub(http, 'patch').resolves(res)
+        spyDelete = sinon.stub(http, 'delete').resolves(res)
+
+        // spyDelete = sinon.stub(http, 'delete').callsFake(() => {
+        //     return Promise.resolve(res)
+        // });
+
+        // spyPost = sinon.stub(http, 'post').callsFake(() => {
+        //     return Promise.resolve(res);
+        // });
 
         spyPut = sinon.stub(http, 'put').callsFake(() => {
             return Promise.resolve(res)
-        })
-    })
+        });
+    });
 
     afterEach(() => {
-        // spy.restore()
+        spy.restore()
         spyPost.restore()
         spyPut.restore()
+        spyPatch.restore()
         spyDelete.restore()
-    })
+    });
 
     describe('getDocument()', () => {
 
-        const documentId = 'document Id'
+        const documentId = 'document Id';
 
         it('Should make a http.get call based on the document Id', async () => {
 
-            const spy = sinon.stub(http, 'get').resolves(res)
-
-            await DMStore.getDocument(documentId)
-            expect(spy).to.be.calledWith(`${url}/documents/${documentId}`)
-
-            spy.restore()
-        })
+            await DMStore.getDocument(documentId);
+            expect(spy).to.be.calledWith(`${url}/documents/${documentId}`);
+        });
 
         it('Should return the data property of the return of a http.get call', async () => {
 
-            const spy = sinon.stub(http, 'get').resolves(res)
-
-            expect(await DMStore.getDocument(documentId)).to.equal('okay')
-
-            spy.restore()
-        })
+            expect(await DMStore.getDocument(documentId)).to.equal('okay');
+        });
 
         /**
+         * TODO:
          * Error Condition if asyncReturnOrError returns a null.
          */
-        it('should return null if asyncReturnOrError returns null', async () => {
+        xit('should return null if asyncReturnOrError returns null', async () => {
 
-            const spy = sinon.stub(http, 'get').resolves(null)
+            spy = sinon.stub(http, 'get').resolves(null);
 
-            expect(await DMStore.getDocument(documentId)).to.equal(null)
+            expect(await DMStore.getDocument(documentId)).to.equal(null);
 
-            spy.restore()
-        })
-    })
+            spy.restore();
+        });
+    });
 
     // TODO: Get this working
     xdescribe('getDocuments()', () => {
 
-        const documentIds = ['42', '42']
+        const documentIds = ['42', '42'];
 
         it('Should make a call to getDocument() for each document Id', async () => {
 
-            const spy = sinon.stub(DMStore, 'getDocument').resolves(res)
+            await DMStore.getDocuments(documentIds);
 
-            await DMStore.getDocuments(documentIds)
-
-            expect(spy).to.be.called
+            expect(spy).to.be.called;
 
             spy.restore()
         })
-    })
+    });
 
     describe('getDocumentVersion()', () => {
 
-        const documentId = 'Document Id'
-        const versionId = 'Version Id'
+        const documentId = 'Document Id';
+        const versionId = 'Version Id';
 
         it('Should make a http.get call based on the document Id', async () => {
 
-            const spy = sinon.stub(http, 'get').resolves(res)
-
             await DMStore.getDocumentVersion(documentId, versionId)
             expect(spy).to.be.calledWith(`${url}/documents/${documentId}/versions/${versionId}`)
-
-            spy.restore()
         })
 
         it('Should return the data property of the return of the http.get call', async () => {
 
-            const spy = sinon.stub(http, 'get').resolves(res)
-
             expect(await DMStore.getDocumentVersion(documentId, versionId)).to.equal('okay')
-
-            spy.restore()
         })
 
         /**
          * Error Condition if asyncReturnOrError returns a null.
          */
-        it('should return null if asyncReturnOrError returns null', async () => {
+        xit('should return null if asyncReturnOrError returns null', async () => {
 
-            const spy = sinon.stub(http, 'get').resolves(null)
+            const spyReturningNull = sinon.stub(http, 'get').resolves(null);
 
-            expect(await DMStore.getDocumentVersion(documentId, versionId)).to.equal(null)
+            expect(await DMStore.getDocumentVersion(documentId, versionId)).to.equal(null);
 
-            spy.restore()
-        })
-    })
+            spyReturningNull.restore();
+        });
+    });
 
     describe('getDocumentBinary()', () => {
 
@@ -140,21 +134,13 @@ describe('DMStore', () => {
 
         it('Should make a http.get call based on the document Id', async () => {
 
-            const spy = sinon.stub(http, 'get').resolves(res)
-
             await DMStore.getDocumentBinary(documentId)
             expect(spy).to.be.calledWith(`${url}/documents/${documentId}/binary`)
-
-            spy.restore()
         })
 
         it('Should return the data property of the return of the http.get call', async () => {
 
-            const spy = sinon.stub(http, 'get').resolves(res)
-
             expect(await DMStore.getDocumentBinary(documentId)).to.equal('okay')
-
-            spy.restore()
         })
     })
 
@@ -165,21 +151,13 @@ describe('DMStore', () => {
 
         it('Should make a http.get call based on the document Id', async () => {
 
-            const spy = sinon.stub(http, 'get').resolves(res)
-
             await DMStore.getDocumentVersionBinary(documentId, versionId)
             expect(spy).to.be.calledWith(`${url}/documents/${documentId}/versions/${versionId}/binary`)
-
-            spy.restore()
         })
 
         it('Should return the data property of the return of the http.get call', async () => {
 
-            const spy = sinon.stub(http, 'get').resolves(res)
-
             expect(await DMStore.getDocumentVersionBinary(documentId, versionId)).to.equal('okay')
-
-            spy.restore()
         })
     })
 
@@ -189,21 +167,13 @@ describe('DMStore', () => {
 
         it('Should make a http.get call based on the document Id', async () => {
 
-            const spy = sinon.stub(http, 'get').resolves(res)
-
             await DMStore.getDocumentThumbnail(documentId)
             expect(spy).to.be.calledWith(`${url}/documents/${documentId}/thumbnail`)
-
-            spy.restore()
         })
 
         it('Should return the data property of the return of the http.get call', async () => {
 
-            const spy = sinon.stub(http, 'get').resolves(res)
-
             expect(await DMStore.getDocumentThumbnail(documentId)).to.equal('okay')
-
-            spy.restore()
         })
     })
 
@@ -214,21 +184,136 @@ describe('DMStore', () => {
 
         it('Should make a http.get call based on the document Id and version Id', async () => {
 
-            const spy = sinon.stub(http, 'get').resolves(res)
-
             await DMStore.getDocumentVersionThumbnail(documentId, versionId)
             expect(spy).to.be.calledWith(`${url}/documents/${documentId}/versions/${versionId}/thumbnail`)
+        });
 
-            spy.restore()
+        it('Should return the data property of the return of the http.get call', async () => {
+
+            expect(await DMStore.getDocumentVersionThumbnail(documentId, versionId)).to.equal('okay')
+        })
+    })
+
+    describe('postDocumentVersion()', () => {
+
+        const documentId = 'Document Id'
+        const file = ''
+        const body = 'body'
+
+        it('Should make a http.post call based on the document Id and version Id', async () => {
+
+            await DMStore.postDocumentVersion(documentId, file, body)
+            expect(spyPost).to.be.calledWith(`${url}/documents/${documentId}`)
+        })
+
+        it('Should return the data property of the return of the http.post call', async () => {
+
+            expect(await DMStore.postDocumentVersion(documentId, file, body)).to.equal('okay')
+        })
+    })
+
+    describe('postDocumentVersionVersion()', () => {
+
+        const documentId = 'Document Id'
+        const file = ''
+        const body = 'body'
+
+        it('Should make a http.post call based on the document Id and version Id', async () => {
+
+            await DMStore.postDocumentVersionVersion(documentId, file, body)
+            expect(spyPost).to.be.calledWith(`${url}/documents/${documentId}`)
+        })
+
+        it('Should return the data property of the return of the http.post call', async () => {
+
+            expect(await DMStore.postDocumentVersionVersion(documentId, file, body)).to.equal('okay')
+        })
+    })
+
+    xdescribe('patchDocument()', () => {
+
+        const documentId = 'Document Id'
+        const updateDocumentCommand = 'test'
+        const body = 'body'
+
+        it('Should make a http.patch call', async () => {
+
+            await DMStore.patchDocument(documentId, updateDocumentCommand, body)
+            expect(spyPatch).to.be.calledWith(`${url}/documents/${documentId}`)
+        })
+
+        it('Should return the data property of the return of the http.patch call', async () => {
+
+            expect(await DMStore.patchDocument(documentId, updateDocumentCommand, body)).to.equal('okay')
+        })
+    })
+
+    describe('deleteDocument()', () => {
+
+        const documentId = 'Document Id'
+
+        it('Should make a http.delete call based on the document Id', async () => {
+
+            await DMStore.deleteDocument(documentId)
+            expect(spyDelete).to.be.calledWith(`${url}/documents/${documentId}`)
+        })
+
+        it('Should return the data property of the return of the http.post call', async () => {
+
+            expect(await DMStore.deleteDocument(documentId)).to.equal('okay')
+        })
+    })
+
+    describe('getDocumentAuditEntries()', () => {
+
+        const documentId = 'Document Id'
+
+        it('Should make a http.get call based on the document Id', async () => {
+
+            await DMStore.getDocumentAuditEntries(documentId)
+            expect(spy).to.be.calledWith(`${url}/documents/${documentId}/auditEntries`)
         })
 
         it('Should return the data property of the return of the http.get call', async () => {
 
-            const spy = sinon.stub(http, 'get').resolves(res)
+            expect(await DMStore.getDocumentAuditEntries(documentId)).to.equal('okay')
+        })
+    })
 
-            expect(await DMStore.getDocumentVersionThumbnail(documentId, versionId)).to.equal('okay')
+    describe('filterDocument()', () => {
 
-            spy.restore()
+        const body = 'body'
+
+        it('Should make a http.post call based on the body', async () => {
+
+            await DMStore.filterDocument(body)
+            expect(spyPost).to.be.calledWith(`${url}/documents/filter`)
+        })
+
+        it('Should return the data property of the return of the http.get call', async () => {
+
+            expect(await DMStore.filterDocument(body)).to.equal('okay')
+        })
+    })
+
+    // TODO: ownedDocument
+
+
+    describe('postDocumentVersionMigrate()', () => {
+
+        const documentId = 'Document Id'
+        const versionId = 'Version Id'
+        const body = 'body'
+
+        it('Should make a http.post call based on the body', async () => {
+
+            await DMStore.postDocumentVersionMigrate(documentId, versionId, body)
+            expect(spyPost).to.be.calledWith(`${url}/documents/${documentId}/versions/${versionId}/migrate`)
+        })
+
+        it('Should return the data property of the return of the http.get call', async () => {
+
+            expect(await DMStore.postDocumentVersionMigrate(documentId, versionId, body)).to.equal('okay')
         })
     })
 })
