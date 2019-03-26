cae7793250593e6ed7e59dd4cb73ca42f6bb52b6
Toireasa Moley <toireasa.moley@hmcts.net>
2019-03-26 12:18:36 +0000

    Mount point

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import * as config from 'config'
-import * as propertiesVolume from '@hmcts/properties-volume'
-propertiesVolume.addTo(config)
+const config = require('@hmcts/properties-volume').addTo(require('config'),{mountPoint:'some/properties/mount/point'})
 
 if (environment.production) {
     enableProdMode();
