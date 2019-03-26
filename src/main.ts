0d6d86c232aa0ad3f5ced868bc38e6db9d4f2146
Toireasa Moley <toireasa.moley@hmcts.net>
2019-03-26 12:03:18 +0000

    Test new config module

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { config } from 'config'
+var config = require('config');
 import * as propertiesVolume from '@hmcts/properties-volume'
 propertiesVolume.addTo(config)
-
-
 
 if (environment.production) {
     enableProdMode();
