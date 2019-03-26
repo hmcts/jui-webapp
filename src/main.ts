e499093c42353025a432555ef2bf7f724d2aabbd
Toireasa Moley <toireasa.moley@hmcts.net>
2019-03-26 11:26:41 +0000

    Adding config file

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import * as config from 'config'
+import { config } from 'config'
 import * as propertiesVolume from '@hmcts/properties-volume'
 propertiesVolume.addTo(config)
 
