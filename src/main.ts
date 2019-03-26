86da0543af7c8c365418f97f38f7fcc35d30a5d6
Toireasa Moley <toireasa.moley@hmcts.net>
2019-03-26 11:21:12 +0000

    Adding properties volume module'

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import * as config from 'config'
+import * as propertiesVolume from '@hmcts/properties-volume'
+propertiesVolume.addTo(config)
 
 
 
