08827552da318856d388aa9c003143a02aba4ffb
Philip <philip.aberneithy@gmail.com>
2019-03-27 16:45:25 +0000

    emNpa tests.

---
+++ api/services/emNpa.spec.ts
@@ -0,0 +1,46 @@
+import * as chai from 'chai'
+import { expect } from 'chai'
+import 'mocha'
+import * as sinon from 'sinon'
+import * as sinonChai from 'sinon-chai'
+import { mockReq, mockRes } from 'sinon-express-mock'
+chai.use(sinonChai)
+
+import { http } from '../lib/http'
+import * as emNpa from './emNpa'
+
+import { config } from '../../config'
+
+describe('emNpa', () => {
+
+    let res
+
+    const url = config.services.em_npa_api
+
+    let spyPost: any
+
+    beforeEach(() => {
+
+        res = {
+            data: 'okay',
+        }
+
+        spyPost = sinon.stub(http, 'post').resolves(res)
+    })
+
+    afterEach(() => {
+
+        spyPost.restore()
+    })
+
+    describe('emNpa', () => {
+
+        it('Should make a post call to get the document tasks', async () => {
+
+            const body = {}
+            await emNpa.createNpaTask(body)
+            expect(spyPost).to.be.calledWith(`${url}/api/document-tasks`)
+        })
+    })
+
+})
