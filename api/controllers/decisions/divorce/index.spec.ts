da17e104fa06c24a538f2fdcbff9c66ebece5d7d
Pardeep Basi <Pardeep.Basi@hmcts.net>
2019-03-27 10:36:54 +0000

    added unit tests and refactoring

--- api/controllers/decisions/divorce/index.spec.ts
+++ api/controllers/decisions/divorce/index.spec.ts
@@ -5,76 +5,195 @@ import * as moment from 'moment'
 import * as sinon from 'sinon'
 import * as sinonChai from 'sinon-chai'
 import { mockReq, mockRes } from 'sinon-express-mock'
+import * as log4jui from '../../../lib/log4jui'
 import * as headerUtilities from '../../../lib/utilities/headerUtilities'
 import * as divorce from './index'
 import * as translateJson from './translate'
+
+const ccdStore = require('../../../services/ccd-store-api/ccd-store')
 
 chai.use(sinonChai)
 
 describe('Decisions - Divorce', () => {
 
     let sandbox
+    const logger = { info: sinon.spy(), error: sinon.spy() }
+    const reqParams = {
+        auth: {
+            userId: 1,
+        },
+        params: {
+            caseId: 1,
+            caseTypeId: 1,
+            jurId: 1,
+            stateId: 1,
+        },
+    }
+    const req = mockReq(reqParams)
+    const state = {
+        caseId: reqParams.params.caseId,
+        caseTypeId: reqParams.params.caseTypeId,
+        jurisdiction: reqParams.params.jurId,
+        stateId: reqParams.params.stateId,
+    }
+    const res = mockRes()
+    const store = {
+        approveDraftConsent: 'yes',
+        notesForAdmin: 'test note',
+    }
 
     beforeEach(() => {
         sandbox = sinon.createSandbox()
+        sandbox.stub(log4jui, 'getLogger').returns(logger)
     })
 
     afterEach(() => {
         sandbox.restore()
     })
 
-    it('should call getAuthHeaders on getOptions', () => {
+    it('getOptions should call getAuthHeaders', () => {
         sandbox.stub(headerUtilities, 'getAuthHeaders')
-        const req = {}
         divorce.getOptions(req)
         expect(headerUtilities.getAuthHeaders).to.have.been.calledWith(req)
     })
 
-    it('should return a data object for CCD on prepareCaseForApproval', () => {
-
-        const store = {
-            notesForAdmin: 'test note',
-        }
-        const momentFormat = 'YYYY-MM-DD'
-        const orderDirection = 'Order Accepted as drafted'
-        const orderDirectionJudge = 'District Judge'
+    it('prepareCaseForApproval should return a data object for CCD', () => {
         const user = { forename: 'Test', surname: 'User' }
-        const orderDirectionJudgeName = `${user.forename} ${user.surname} `
-        const eventId = 'eventId'
-        const eventToken = 'token'
 
-        sandbox.stub(moment.fn, 'format').callsFake(() => momentFormat)
+        const caseForApproval = {
+            data: {
+                orderDirection: 'Order Accepted as drafted',
+                orderDirectionAddComments: store.notesForAdmin,
+                orderDirectionDate: 'YYYY-MM-DD',
+                orderDirectionJudge: 'District Judge',
+                orderDirectionJudgeName: `${user.forename} ${user.surname} `,
+            },
+            event: {
+                id: 'eventId',
+            },
+            event_token: 'token',
 
-        const result = divorce.prepareCaseForApproval(eventToken, eventId, user, store)
+            ignore_warning: true,
+        }
 
-        // test data
-        expect(result).to.nested.include({ 'data.orderDirection': orderDirection })
-        expect(result).to.nested.include({ 'data.orderDirectionAddComments': store.notesForAdmin })
-        expect(result).to.nested.include({ 'data.orderDirectionDate': momentFormat })
-        expect(result).to.nested.include({ 'data.orderDirectionJudge': orderDirectionJudge })
-        expect(result).to.nested.include({ 'data.orderDirectionJudgeName': orderDirectionJudgeName })
+        sandbox.stub(moment.fn, 'format').callsFake(() => 'YYYY-MM-DD')
 
-        // test event
-        expect(result).to.nested.include({ 'event.id': eventId })
+        const result = divorce.prepareCaseForApproval(caseForApproval.event_token, caseForApproval.event.id, user, store)
 
-        // test event_token
-        expect(result).to.nested.include({ 'event_token': eventToken })
-
-        // test ignore_warning
-        expect(result).to.nested.include({ 'ignore_warning': true })
+        expect(result).to.deep.equal(caseForApproval)
 
     })
 
-    it('should perform a lookup upon translate if field exists in store', () => {
-        const fieldName = Object.keys(translateJson.lookup).pop()
-        const result = divorce.translate(translateJson.lookup, fieldName)
-        expect(result).to.equals(translateJson.lookup[fieldName])
+    describe('translate', () => {
+        it('should perform a lookup if field exists in store', () => {
+            const fieldName = Object.keys(translateJson.lookup).pop()
+            const result = divorce.translate(translateJson.lookup, fieldName)
+            expect(result).to.equals(translateJson.lookup[fieldName])
+        })
+
+        it('should return null if field doesn\'t exist in store', () => {
+            const fieldName = 'test'
+            // smoke test in case fieldName does actually exist
+            expect(translateJson.lookup[fieldName]).to.be.undefined
+            const result = divorce.translate(translateJson.lookup, fieldName)
+            expect(result).to.be.null
+        })
     })
 
-    it('should return null upon translate if field doesn\'t exist in store', () => {
-        const fieldName = 'test'
-        expect(translateJson.lookup[fieldName]).to.be.undefined
-        const result = divorce.translate(translateJson.lookup, fieldName)
-        expect(result).to.be.null
+    describe('getEventTokenForMakeDecision', () => {
+
+        let result
+
+        beforeEach(() => {
+            sandbox.stub(divorce, 'getOptions').returns('getOptionsReturn')
+        })
+
+        describe('success', () => {
+            beforeEach(() => {
+                sandbox.stub(ccdStore, 'getEventTokenAndCase').resolves({
+                    token: 'eventToken',
+                })
+            })
+
+            it('should log the eventToken', async () => {
+                result = await divorce.getEventTokenForMakeDecision(store.approveDraftConsent, req, state)
+                expect(logger.info).to.have.been.calledWith('Got token eventToken')
+            })
+
+            it('should return the eventToken on decision == yes', async () => {
+                result = await divorce.getEventTokenForMakeDecision(store.approveDraftConsent, req, state)
+                expect(result).to.equals('eventToken')
+            })
+
+            it('should return the eventToken on decision == no', async () => {
+                result = await divorce.getEventTokenForMakeDecision('no', req, state)
+                expect(result).to.equals('eventToken')
+            })
+        })
+
+        describe('exceptions', () => {
+            beforeEach(async () => {
+                sandbox.stub(ccdStore, 'getEventTokenAndCase').throws('Invalid token')
+                result = await divorce.getEventTokenForMakeDecision(store.approveDraftConsent, req, state)
+            })
+
+            it('should log errors on exception', () => {
+
+                expect(logger.info).to.have.been.calledWith('Getting Event Token')
+                expect(logger.error).to.have.been.calledWith('Error getting event token')
+            })
+
+            it('should return false on exception', () => {
+
+                expect(ccdStore.getEventTokenAndCase).to.have.been.calledWith(
+                    reqParams.auth.userId,
+                    'DIVORCE',
+                    'FinancialRemedyMVP2',
+                    state.caseId,
+                    'FR_approveApplication',
+                    'getOptionsReturn'
+                )
+                expect(result).to.be.false
+            })
+        })
+    })
+
+    describe('payload.divorce', () => {
+        it('should not have a payload', async () => {
+            const result = await divorce.payload.divorce()
+            expect(result).to.be.undefined
+        })
+    })
+
+    describe('payload.financialremedymvp2', () => {
+
+        let makeDecisionStub
+
+        beforeEach(() => {
+            makeDecisionStub = sandbox.stub(divorce, 'makeDecision').resolves('test')
+        })
+
+        it('should log POST call', async () => {
+            await divorce.payload.financialremedymvp2(req, res, store)
+            expect(logger.info).to.have.been.calledWith('Posting to CCD')
+            expect(logger.info).to.have.been.calledWith('Posted to CCD', 'test')
+        })
+
+        it('should return decision-confirmation', async () => {
+            const result = await divorce.payload.financialremedymvp2(req, res, store)
+            expect(divorce.makeDecision).to.have.been.calledWith(store.approveDraftConsent, req, state, store)
+            expect(result).to.equals('decision-confirmation')
+        })
+
+        it('should set 400 error status and return null if not a valid decision', async () => {
+
+            makeDecisionStub.restore()
+            sandbox.stub(divorce, 'makeDecision').resolves(false)
+
+            const result = await divorce.payload.financialremedymvp2(req, res, store)
+            expect(result).to.be.null
+            expect(res.status).to.be.calledWith(400)
+            expect(res.send).to.be.calledWith('Error updating case')
+        })
     })
 })
--- api/controllers/decisions/divorce/index.ts
+++ api/controllers/decisions/divorce/index.ts
@@ -1,4 +1,5 @@
 import * as exceptionFormatter from 'exception-formatter'
+import { Request, Response } from 'express'
 import { config } from '../../../../config'
 import * as log4jui from '../../../lib/log4jui'
 
@@ -14,8 +15,6 @@ export const mapping = Mapping.mapping
 export const templates = Templates.templates
 
 const ccdStore = require('../../../services/ccd-store-api/ccd-store')
-
-const logger = log4jui.getLogger('State')
 
 const ERROR400 = 400
 const exceptionOptions = {
@@ -52,27 +51,9 @@ export function translate(store, fieldName) {
     return null
 }
 
-// function perpareCaseForConsentOrder(documentAnnotationId, eventToken, eventId, user, store) {
-//     const payload = {
-//         /* eslint-disable-next-line id-blacklist */
-//         data: {
-//             consentOrder: {
-//                 document_url: `${config.services.dm_store_api}/documents/${documentAnnotationId}`,
-//                 document_binary_url: `${config.services.dm_store_api}//documents/${documentAnnotationId}/binary`
-//             }
-//         },
-//         event: {
-//             id: eventId
-//         },
-//         event_token: eventToken,
-
-//         ignore_warning: true
-//     }
-
-//     return payload
-// }
-
 export function prepareCaseForRefusal(eventToken, eventId, user, store) {
+    const logger = log4jui.getLogger('State')
+
     let orderRefusal = []
     let orderRefusalOther = null
     let orderRefusalNotEnough = []
@@ -88,7 +69,7 @@ export function prepareCaseForRefusal(eventToken, eventId, user, store) {
     orderRefusal.push(translate(store, 'd81'))
     orderRefusal.push(translate(store, 'pensionAnnex'))
     orderRefusal.push(translate(store, 'applicantTakenAdvice'))
-    orderRefusal.push(translate(store, 'respondentTakenAdvice'))
+    orderRefusal.push(translate(store,  'respondentTakenAdvice'))
 
     if (store.partiesNeedAttend) {
         estimateLengthOfHearing = store.estimateLengthOfHearing
@@ -170,31 +151,38 @@ export function prepareCaseForRefusal(eventToken, eventId, user, store) {
     }
 }
 
-export async function makeDecision(decision, req, state, store) {
-    let payloadData = {}
+export async function getEventTokenForMakeDecision(decision: string, req: Request, state): Promise<any> {
+    const logger = log4jui.getLogger('State')
     let eventToken = {}
-
     try {
         logger.info('Getting Event Token')
 
         const event = decision === 'yes' ? 'FR_approveApplication' : 'FR_orderRefusal'
 
-        const eventTokenAndCAse = await ccdStore.getEventTokenAndCase(
+        const eventTokenAndCase = await ccdStore.getEventTokenAndCase(
             req.auth.userId,
             'DIVORCE',
             'FinancialRemedyMVP2',
             state.caseId,
             event,
-            getOptions(req)
+            this.getOptions(req)
         )
 
-        eventToken = eventTokenAndCAse.token
+        eventToken = eventTokenAndCase.token
 
         logger.info(`Got token ${eventToken}`)
+        return eventToken
     } catch (exception) {
         logger.error('Error getting event token', exceptionFormatter(exception, exceptionOptions))
         return false
     }
+}
+
+export async function getPayloadDataForMakeDecision(decision, req, state, store) {
+    const eventToken = await this.getEventTokenForMakeDecision(decision, req, state)
+    const logger = log4jui.getLogger('State')
+
+    let payloadData
 
     if (decision === 'yes') {
         payloadData = prepareCaseForApproval(eventToken, 'FR_approveApplication', req.session.user, store)
@@ -204,9 +192,17 @@ export async function makeDecision(decision, req, state, store) {
         payloadData = prepareCaseForRefusal(eventToken, 'FR_orderRefusal', req.session.user, store)
     }
 
+    logger.info('Payload assembled')
+    logger.info(JSON.stringify(payloadData))
+
+    return payloadData
+}
+
+export async function makeDecision(decision: string, req, state, store): Promise<boolean> {
+    const logger = log4jui.getLogger('State')
+    const payloadData = await this.getPayloadDataForMakeDecision(decision, req, state, store)
+
     try {
-        logger.info('Payload assembled')
-        logger.info(JSON.stringify(payloadData))
         await ccdStore.postCaseWithEventToken(
             req.auth.userId,
             'DIVORCE',
@@ -215,7 +211,6 @@ export async function makeDecision(decision, req, state, store) {
             payloadData,
             getOptions(req)
         )
-
         return true
     } catch (exception) {
         logger.error('Error sending event', exceptionFormatter(exception, exceptionOptions))
@@ -225,7 +220,7 @@ export async function makeDecision(decision, req, state, store) {
 
 export const payload: any = []
 
-payload.divorce = async (req, res, store) => {
+payload.divorce = async () => {
     //no payload
 }
 
@@ -234,6 +229,8 @@ payload.financialremedymvp2 = async (req, res, store) => {
     const caseId = req.params.caseId
     const caseTypeId = req.params.caseTypeId
     const stateId = req.params.stateId
+
+    const logger = log4jui.getLogger('State')
 
     const state = {
         caseId,
@@ -251,7 +248,6 @@ payload.financialremedymvp2 = async (req, res, store) => {
         return 'decision-confirmation'
     }
 
-    res.status(ERROR400)
-    res.send('Error updating case')
+    res.status(ERROR400).send('Error updating case')
     return null
 }
