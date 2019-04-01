513c2e8061a3084c9dabcedeea305af93c0803d3
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2019-04-01 12:47:03 +0100

    ccd_def_changed_aat_demo

--- config/environments/demo.config.ts
+++ config/environments/demo.config.ts
@@ -3,7 +3,7 @@ export default {
         ccd_data_api:
             'https://ccd-data-store-api-demo.service.core-compute-demo.internal',
         ccd_def_api:
-            'https://ccd-definition-store-api-aat.service.core-compute-aat.internal',
+            'https://ccd-definition-store-api-demo.service.core-compute-demo.internal',
         idam_web: 'https://idam-web-public.demo.platform.hmcts.net',
         idam_api: 'https://idam-api.demo.platform.hmcts.net',
         s2s:
