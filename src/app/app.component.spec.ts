2fb506c05a26623f18a966a8fce9e13185367600
Pardeep Singh Basi <paddybasi@gmail.com>
2019-08-21 16:47:36 +0100

    added tests for new code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,17 @@
 import { TestBed, async, fakeAsync, tick } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Renderer2 } from '@angular/core';
 import {ConfigService} from './config.service';
 import {configMock} from './domain/services/mock/config.env.mock';
 import {RouterTestingModule} from '@angular/router/testing';
-import { Router } from '@angular/router';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+
 describe('AppComponent', () => {
 
     let app;
     let fixture;
     let router: Router;
+    let renderer: Renderer2;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -18,6 +20,7 @@ describe('AppComponent', () => {
                 AppComponent
             ],
             providers: [
+                Renderer2,
                 {
                     provide: ConfigService,
                     useValue: configMock
@@ -26,11 +29,19 @@ describe('AppComponent', () => {
             schemas: [CUSTOM_ELEMENTS_SCHEMA]
         }).compileComponents();
 
+    }));
+
+    beforeEach( () => {
         fixture = TestBed.createComponent(AppComponent);
         app = fixture.debugElement.componentInstance;
         router = TestBed.get(Router);
 
-    }));
+        renderer = fixture.componentRef.injector.get(Renderer2);
+        // and spy on it
+        spyOn(renderer, 'addClass').and.callThrough();
+        spyOn(renderer, 'removeClass').and.callThrough();
+    });
+
     it('should create the app', async(() => {
         const component = fixture.componentInstance;
         expect(component).toBeTruthy();
@@ -54,4 +65,50 @@ describe('AppComponent', () => {
         tick();
         expect(spy).toHaveBeenCalled();
     }));
+
+    it('should add the loading class if loading is true', async( () => {
+        expect(renderer.addClass).not.toHaveBeenCalled();
+        app.loading = true;
+        expect(renderer.addClass).toHaveBeenCalledWith(jasmine.any(Object), app.loadingClass);
+    }));
+
+    it('should remove the loading class if loading is false', async( () => {
+        expect(renderer.removeClass).not.toHaveBeenCalled();
+        app.loading = false;
+        expect(renderer.removeClass).toHaveBeenCalledWith(jasmine.any(Object), app.loadingClass);
+    }));
+
+    describe('navigationInterceptor', () => {
+
+        let spy;
+
+        beforeEach( () => {
+            spy = spyOnProperty(app, 'loading', 'set');
+        });
+
+        it('should set loading state to true if navigation start', async( () => {
+            const ne = new NavigationStart(0, 'http://localhost:4200', 'imperative');
+            app.navigationInterceptor(ne);
+            expect(spy).toHaveBeenCalledWith(true);
+        }));
+
+        it('should set loading state to false if navigation end', async( () => {
+            const ne = new NavigationEnd(0, 'http://localhost:4200', 'http://localhost:4200/login');
+            app.navigationInterceptor(ne);
+            expect(spy).toHaveBeenCalledWith(false);
+        }));
+
+        it('should set loading state to false if navigation cancel', async( () => {
+            const ne = new NavigationCancel(0, 'http://localhost:4200/login', 'cancelled');
+            app.navigationInterceptor(ne);
+            expect(spy).toHaveBeenCalledWith(false);
+        }));
+
+        it('should set loading state to false if navigation error', async( () => {
+            const ne = new NavigationError(0, 'http://localhost:4200/login', 'error');
+            app.navigationInterceptor(ne);
+            expect(spy).toHaveBeenCalledWith(false);
+        }));
+    });
+
 });
