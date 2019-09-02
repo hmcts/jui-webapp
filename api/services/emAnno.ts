4328d918b81317dd112e087fb6ba7a9a270229b9
Pardeep Singh Basi <paddybasi@gmail.com>
2019-09-02 15:01:47 +0100

    updated to media viewer latest and fixed some issues

--- api/services/emAnno.ts
+++ api/services/emAnno.ts
@@ -39,7 +39,7 @@ export async function handlePost(req: Request, res: Response): Promise<void> {
 }
 
 export async function handleGet(req: Request, res: Response): Promise<void> {
-    const uuid = req.params.uuid
+    const uuid = req.params.documentId
     const response = await asyncReturnOrError(getAnnotionSet(uuid), ' Error getting annotations', res, logger)
 
     if (response) {
@@ -69,7 +69,7 @@ export default async app => {
     app.use('/em-anno', router)
 
     router.post('/annotation-sets', await handlePost)
-    router.get('/annotation-sets/:uuid', await handleGet)
+    router.get('/annotation-sets/filter', await handleGet)
     router.delete('/annotations/:uuid', await handleDelete)
     router.post('/annotations', await handleAdd)
 }
