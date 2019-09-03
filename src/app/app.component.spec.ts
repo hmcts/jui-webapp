de835595c7e119eb3f2dcdd624ecae28f264f4bc
Pardeep Singh Basi <paddybasi@gmail.com>
2019-09-03 08:18:21 +0100

    added more test coverage

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, fakeAsync, tick } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { CUSTOM_ELEMENTS_SCHEMA, Renderer2 } from '@angular/core';
 import {ConfigService} from './config.service';
@@ -41,31 +41,47 @@ describe('AppComponent', () => {
         spyOn(renderer, 'removeClass').and.callThrough();
     });
 
-    it('should create the app', async(() => {
+    it('should create the app', () => {
         const component = fixture.componentInstance;
         expect(component).toBeTruthy();
         expect(component).not.toBeNull();
-    }));
-    it(`should have as title 'app'`, async(() => {
-        expect(app.title).toEqual('JUI Web App');
-    }));
-    it(`should replace title`, async(() => {
-        expect(['summary', 'parties', 'casefile', 'timeline', 'decision', 'hearing', 'reject-reasons', '/']).toContain(app.replacedTitles('test'));
-    }));
-    it(`should return title mapping `, async(() => {
-        expect('Your cases - Judicial case manager').toContain(app.getTitle('/'));
-    }));
+    });
 
-    it('should add the loading class if loading is true', async( () => {
+    it(`should have as title 'app'`, () => {
+        expect(app.title).toEqual('JUI Web App');
+    });
+
+    it(`should replace title`, () => {
+        expect(['summary', 'parties', 'casefile', 'timeline', 'decision', 'hearing', 'reject-reasons', '/']).toContain(app.replacedTitles('test'));
+    });
+
+    it(`should return title mapping `, () => {
+        expect('Your cases - Judicial case manager').toContain(app.getTitle('/'));
+    });
+
+    it('should add the loading class if loading is true', () => {
         expect(renderer.addClass).not.toHaveBeenCalled();
         app.loading = true;
         expect(renderer.addClass).toHaveBeenCalledWith(jasmine.any(Object), app.loadingClass);
-    }));
+    });
 
-    it('should remove the loading class if loading is false', async( () => {
+    it('should remove the loading class if loading is false', () => {
         expect(renderer.removeClass).not.toHaveBeenCalled();
         app.loading = false;
         expect(renderer.removeClass).toHaveBeenCalledWith(jasmine.any(Object), app.loadingClass);
+    });
+
+    it('should subscribe to router events and call replacedTitles on navigation end', fakeAsync(() => {
+        const title = 'Your cases - Judicial case manager';
+        spyOn(app, 'replacedTitles').and.returnValue('/');
+        spyOn(app, 'getTitle').and.returnValue(title);
+
+        app.ngOnInit();
+        router.navigate(['/']);
+        tick();
+        expect(app.replacedTitles).toHaveBeenCalledWith('/');
+        expect(app.getTitle).toHaveBeenCalledWith('/');
+        expect(app.title).toBe(title);
     }));
 
 });
