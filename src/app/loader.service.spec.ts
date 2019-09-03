3591515ce79f1840d7b93fae9d3dd17d8c24ea5a
Pardeep Singh Basi <paddybasi@gmail.com>
2019-09-03 09:27:56 +0100

    added more test coverage

--- src/app/loader.service.spec.ts
+++ src/app/loader.service.spec.ts
@@ -1,7 +1,9 @@
-import {inject, TestBed} from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 import { LoaderService } from './loader.service';
 
 describe('Service: LoaderService', () => {
+
+    let service: LoaderService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -11,14 +13,23 @@ describe('Service: LoaderService', () => {
                 LoaderService
             ]
         });
-    });
-    it('should be created', inject([LoaderService], (service: LoaderService) => {
-        expect(service).toBeTruthy();
-    }));
 
-    it('should have initial loading state of false', inject([LoaderService], (service: LoaderService) => {
+        service = TestBed.get(LoaderService);
+    });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should have initial loading state of false', () => {
         service.isLoading.subscribe( loading => {
             expect(loading).toBeFalsy();
         });
-    }));
+    });
+
+    it('should emit the correct values before subscription', () => {
+        service.isLoading.next(true);
+        service.isLoading.subscribe( loading => {
+            expect(loading).toBeTruthy();
+        });
+    });
 });
