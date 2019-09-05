244b2ab3e107e4acc0e34a95677acdce13cf7f22
Philip <philip.aberneithy@gmail.com>
2019-09-05 10:34:04 +0100

    Checking if this ignore works.

--- src/app/routing/pages/page-not-found/page-not-found.component.ts
+++ src/app/routing/pages/page-not-found/page-not-found.component.ts
@@ -1,11 +1,16 @@
 import {Component} from '@angular/core';
 
+/**
+ * @Component
+ */
 @Component({
     selector: 'app-page-not-found',
     templateUrl: './page-not-found.component.html',
     styleUrls: ['./page-not-found.component.scss']
 })
-//IgnoreTest
+/**
+ * @ComponentEnd
+ */
 export class PageNotFoundComponent {
     constructor() {
     }
